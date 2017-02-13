537b32389af30d6743bd729f3c7ae405fa2b05f9
Marc Shilling <mshilling@mindgrub.com>
2017-02-13 16:33:42 -0500

    dont copy unnecessary files to bundle

--- Mobile/Configuration/environment_preprocess.h
+++ Mobile/Configuration/environment_preprocess.h
@@ -1,8 +1,8 @@
 //-----------------------------------------
 // Auto generated file
-// Created 2017-02-13 16:19:20
+// Created 2017-02-13 16:33:17
 //-----------------------------------------
 
 #define EXM_ENVIRONMENT              DEVELOP
-#define EXM_ENVIRONMENT_LAST_COMMIT  89d9b5c
+#define EXM_ENVIRONMENT_LAST_COMMIT  ef2fa9c
 #define EXM_ENVIRONMENT_APP_NAME     BGE_DEV 13
