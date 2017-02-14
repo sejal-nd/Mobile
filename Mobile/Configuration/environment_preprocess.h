f79ee54b4f72117ae58ef41dd0edef2716c25580
Kenny Roethel <kenny@mindgrub.com>
2017-02-14 18:02:16 -0500

    Remove bridge header for sdk (not implemented yet) - remove preprocess file

--- Mobile/Configuration/environment_preprocess.h
+++
@@ -1,8 +0,0 @@
-//-----------------------------------------
-// Auto generated file
-// Created 2017-02-14 17:09:26
-//-----------------------------------------
-
-#define EXM_ENVIRONMENT              DEVELOP
-#define EXM_ENVIRONMENT_LAST_COMMIT  f1ebd65
-#define EXM_ENVIRONMENT_APP_NAME     BGE_DEV 14
