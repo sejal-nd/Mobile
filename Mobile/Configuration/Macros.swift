e9739ff6efe9e4c1fbc2f851905e913cc5f861e6
Kenny Roethel <kenny@mindgrub.com>
2017-03-10 18:08:30 -0500

    Adds debug logging macro

---
+++ Mobile/Configuration/Macros.swift
@@ -0,0 +1,15 @@
+//
+//  Macros.swift
+//  Mobile
+//
+//  Created by Kenny Roethel on 3/10/17.
+//  Copyright © 2017 Exelon Corporation. All rights reserved.
+//
+
+import Foundation
+
+func dLog(message: String, filename: String = #file, function: String = #function, line: Int = #line) {
+    #if DEBUG
+        NSLog("[\((filename as NSString).lastPathComponent):\(line)] \(function) - \(message)")
+    #endif
+}
