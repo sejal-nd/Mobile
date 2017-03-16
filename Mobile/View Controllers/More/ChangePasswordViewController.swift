a2e1738c71b986510dcbd2e21c20af796cba5190
Marc Shilling <mshilling@mindgrub.com>
2017-03-16 17:59:34 -0400

    fixed EM-1194 failing

--- Mobile/View Controllers/More/ChangePasswordViewController.swift
+++ Mobile/View Controllers/More/ChangePasswordViewController.swift
@@ -228,6 +228,9 @@ class ChangePasswordViewController: UIViewController, UITextFieldDelegate {
     
     // Don't allow whitespace entry in the newPasswordTextField
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if string.characters.count == 0 { // Allow backspace
+            return true
+        }
         if string.trimmingCharacters(in: .whitespacesAndNewlines).characters.count == 0 {
             return false
         }
