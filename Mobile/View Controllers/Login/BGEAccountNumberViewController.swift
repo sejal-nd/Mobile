7142444105f035744122f8de3ba6d47add290e15
Marc Shilling <mshilling@mindgrub.com>
2017-04-07 17:19:08 -0400

    ui complete

---
+++ Mobile/View Controllers/Login/BGEAccountNumberViewController.swift
@@ -0,0 +1,63 @@
+//
+//  BGEAccountNumberViewController.swift
+//  Mobile
+//
+//  Created by Marc Shilling on 4/7/17.
+//  Copyright © 2017 Exelon Corporation. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class BGEAccountNumberViewController: UIViewController {
+    
+    let viewModel = ForgotUsernameViewModel()
+
+    @IBOutlet weak var instructionLabel: UILabel!
+    @IBOutlet weak var accountNumberTextField: FloatLabelTextField!
+    
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = NSLocalizedString("Forgot Username", comment: "")
+        
+        let nextButton = UIBarButtonItem(title: NSLocalizedString("Next", comment: ""), style: .done, target: self, action: #selector(onNextPress))
+        navigationItem.rightBarButtonItem = nextButton
+        viewModel.accountNumberNotEmpty().bindTo(nextButton.rx.isEnabled).addDisposableTo(disposeBag)
+        
+        view.backgroundColor = .primaryColor
+        
+        instructionLabel.textColor = .darkJungleGreen
+        instructionLabel.text = NSLocalizedString("The information entered is associated with multiple accounts. Please enter the account number you would like to proceed with.", comment: "")
+
+        accountNumberTextField.textField.placeholder = NSLocalizedString("Account Number*", comment: "")
+        accountNumberTextField.textField.autocorrectionType = .no
+        //accountNumberTextField.textField.keyboardType = .numberPad
+        accountNumberTextField.textField.returnKeyType = .done
+        accountNumberTextField.textField.isShowingAccessory = true
+        accountNumberTextField.textField.rx.text.orEmpty.bindTo(viewModel.accountNumber).addDisposableTo(disposeBag)
+    }
+
+    func onNextPress() {
+        view.endEditing(true)
+        
+        viewModel.validateAccount(onSuccess: { 
+            print("success!")
+        }, onNeedAccountNumber: {
+            // wont happen?
+        }, onError: { errorMessage in
+            let alertController = UIAlertController(title: NSLocalizedString("Error", comment: ""), message: errorMessage, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+        })
+    }
+    
+    @IBAction func onAccountNumberTooltipPress() {
+        print("account number tooltip")
+    }
+
+
+
+}
---
+++ Mobile/View Models/ForgotUsernameViewModel.swift
@@ -0,0 +1,66 @@
+//
+//  ForgotUsernameViewModel.swift
+//  Mobile
+//
+//  Created by Marc Shilling on 4/7/17.
+//  Copyright © 2017 Exelon Corporation. All rights reserved.
+//
+
+import RxSwift
+
+class ForgotUsernameViewModel {
+    let disposeBag = DisposeBag()
+    
+    let phoneNumber = Variable("")
+    let identifierNumber = Variable("")
+    let accountNumber = Variable("")
+    
+    func validateAccount(onSuccess: @escaping () -> Void, onNeedAccountNumber: @escaping () -> Void, onError: @escaping (String) -> Void) {
+        print("Phone number: \(extractDigitsFrom(phoneNumber.value))")
+        print("Identifier number: \(identifierNumber.value)")
+        print("Account number: \(accountNumber.value)")
+        
+        //onError(NSLocalizedString("The information entered does not match our records. Please try again.", comment: ""))
+        if accountNumber.value.characters.count > 0 {
+            onSuccess()
+        } else {
+            onNeedAccountNumber()
+        }
+        
+    }
+    
+    func nextButtonEnabled() -> Observable<Bool> {
+        if Environment.sharedInstance.opco == "BGE" {
+            return Observable.combineLatest(phoneNumberHasTenDigits(), identifierHasFourDigits()) {
+                return $0 && $1
+            }
+        } else {
+            return Observable.combineLatest(phoneNumberHasTenDigits(), accountNumberNotEmpty()) {
+                return $0 && $1
+            }
+        }
+    }
+    
+    func phoneNumberHasTenDigits() -> Observable<Bool> {
+        return phoneNumber.asObservable().map({ text -> Bool in
+            let digitsOnlyString = self.extractDigitsFrom(text)
+            return digitsOnlyString.characters.count == 10
+        })
+    }
+    
+    func identifierHasFourDigits() -> Observable<Bool> {
+        return identifierNumber.asObservable().map({ text -> Bool in
+            return text.characters.count == 4
+        })
+    }
+    
+    func accountNumberNotEmpty() -> Observable<Bool> {
+        return accountNumber.asObservable().map({ text -> Bool in
+            return text.characters.count > 0
+        })
+    }
+    
+    private func extractDigitsFrom(_ string: String) -> String {
+        return string.components(separatedBy: NSCharacterSet.decimalDigits.inverted).joined(separator: "")
+    }
+}
