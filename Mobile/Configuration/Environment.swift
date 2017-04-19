c5af798431f04c0d9edd12f1c6ed0ca8db8ea0be
Marc Shilling <mshilling@mindgrub.com>
2017-04-19 15:54:48 -0400

    resolve EM-1438

--- Mobile/Configuration/Environment.swift
+++ Mobile/Configuration/Environment.swift
@@ -35,10 +35,10 @@ struct Environment  {
             formatter.dateFormat = "MM/dd/yyyy hh:mm a"
             break
         case "ComEd":
-            formatter.dateFormat = "hh:mm a 'on' MM/dd/yyyy"
+            formatter.dateFormat = "hh:mm a 'on' M/dd/yyyy"
             break
         case "PECO":
-            formatter.dateFormat = "h:mm a zz 'on' MM/dd/yyyy"
+            formatter.dateFormat = "h:mm a zz 'on' M/dd/yyyy"
             break
         default:
             formatter.dateFormat = "h:mm a MM/dd/yy"
