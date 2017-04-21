6df20b0746819eaf7d391135ce5dfe5c93156196
Sam Francis <sfrancis@mindgrub.com>
2017-04-21 17:59:02 -0400

    Added the paperless ebill view model class.

---
+++ Mobile/View Models/PaperlessEBillViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  PaperlessEBillViewModel.swift
+//  Mobile
+//
+//  Created by Sam Francis on 4/21/17.
+//  Copyright © 2017 Exelon Corporation. All rights reserved.
+//
+
+import Foundation
+
+class PaperlessEBillViewModel {
+    
+    init() {
+        
+    }
+    
+}
