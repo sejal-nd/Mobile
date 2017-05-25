de1187375e4a6822f0aa38b813eec05d7082dde2
Marc Shilling <mshilling@mindgrub.com>
2017-05-24 23:16:46 -0400

    tablet layout for root wallet screen

--- Mobile/Views/Table View Cells/WalletTableViewCell.swift
+++ Mobile/Views/Table View Cells/WalletTableViewCell.swift
@@ -21,7 +21,7 @@ class WalletTableViewCell: UITableViewCell {
     @IBOutlet private weak var bottomBarView: UIView!
     @IBOutlet weak var bottomBarLabel: UILabel!
     
-    var gradientLayer: CAGradientLayer!
+    var gradientLayer = CAGradientLayer()
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -34,15 +34,7 @@ class WalletTableViewCell: UITableViewCell {
         innerContentView.layer.cornerRadius = 5
         
         gradientView.layer.cornerRadius = 5
-        gradientLayer = CAGradientLayer()
         gradientLayer.frame = gradientView.bounds
-        
-        // Round only the top corners
-        var path = UIBezierPath(roundedRect:gradientLayer.bounds, byRoundingCorners:[.topLeft, .topRight], cornerRadii: CGSize(width: 5, height:  5))
-        var maskLayer = CAShapeLayer()
-        maskLayer.path = path.cgPath
-        gradientLayer.mask = maskLayer
-        
         gradientLayer.colors = [
             UIColor.white.cgColor,
             UIColor(red: 238/255, green: 242/255, blue: 248/255, alpha: 1).cgColor
@@ -55,19 +47,34 @@ class WalletTableViewCell: UITableViewCell {
         
         bottomBarShadowView.addShadow(color: .black, opacity: 0.1, offset: .zero, radius: 2)
         
-        // Round only the bottom corners
-        path = UIBezierPath(roundedRect:bottomBarView.bounds, byRoundingCorners:[.bottomLeft, .bottomRight], cornerRadii: CGSize(width: 5, height:  5))
-        maskLayer = CAShapeLayer()
-        maskLayer.path = path.cgPath
-        bottomBarView.layer.mask = maskLayer
         bottomBarView.addShadow(color: .black, opacity: 0.1, offset: .zero, radius: 2)
         
         bottomBarLabel.textColor = .blackText
     }
-    
+        
     override func setHighlighted(_ highlighted: Bool, animated: Bool) {
         // disable highlight
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.layoutIfNeeded() // Needed for frames to be correct
+        
+        // Round only the top corners
+        gradientLayer.frame = gradientView.frame
+        let gradientPath = UIBezierPath(roundedRect:gradientLayer.bounds, byRoundingCorners:[.topLeft, .topRight], cornerRadii: CGSize(width: 5, height:  5))
+        let gradientMaskLayer = CAShapeLayer()
+        gradientMaskLayer.path = gradientPath.cgPath
+        gradientLayer.mask = gradientMaskLayer
+        
+        // Round only the bottom corners
+        let bottomBarPath = UIBezierPath(roundedRect:bottomBarView.bounds, byRoundingCorners:[.bottomLeft, .bottomRight], cornerRadii: CGSize(width: 5, height:  5))
+        let bottomBarMaskLayer = CAShapeLayer()
+        bottomBarMaskLayer.path = bottomBarPath.cgPath
+        bottomBarView.layer.mask = bottomBarMaskLayer
+    }
+    
     
     func bindToWalletItem(_ walletItem: WalletItem) {
         
