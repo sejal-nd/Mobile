409367ae2e28b0b49197fad50467e60840323ba7
Marc Shilling <mshilling@mindgrub.com>
2017-06-16 10:50:21 -0400

    created AccountInfoBar. Use it in PECO release of info. Fix crash there related to keep me signed in

---
+++ Mobile/Views/AccountInfoBar.swift
@@ -0,0 +1,66 @@
+//
+//  AccountInfoBar.swift
+//  Mobile
+//
+//  Created by Marc Shilling on 6/16/17.
+//  Copyright © 2017 Exelon Corporation. All rights reserved.
+//
+
+import UIKit
+
+class AccountInfoBar: UIView {
+    
+    var label: UILabel!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        commonInit()
+    }
+    
+    func commonInit() {
+        backgroundColor = .softGray
+        
+        label = UILabel(frame: .zero)
+        label.numberOfLines = 2
+        label.textColor = .deepGray
+        label.font = SystemFont.medium.of(textStyle: .subheadline)
+        label.textAlignment = .center
+
+        update()
+        
+        addSubview(label)
+    }
+    
+    func update() {
+        if let currentAccount = AccountsStore.sharedInstance.currentAccount {
+            var labelText = "ACCOUNT \(currentAccount.accountNumber)"
+            if let address = currentAccount.address {
+                labelText.append("\n\(address)")
+            }
+            label.text = labelText
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        addBottomBorder(color: .accentGray, width: 1)
+    }
+    
+    override func updateConstraints() {
+        super.updateConstraints()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8).isActive = true
+        label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 8).isActive = true
+    }
+}
