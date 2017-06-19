c06003fdc173f080ca69194ca856de47d9a3ed53
Sam Francis <sfrancis@mindgrub.com>
2017-06-19 18:23:46 -0400

    Stubbed out the enroll in autopay view.

--- Mobile/View Controllers/Bill/AutoPay/AutoPayViewController.swift
+++ Mobile/View Controllers/Bill/AutoPay/AutoPayViewController.swift
@@ -7,8 +7,23 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class AutoPayViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var gradientView: UIView!
+    var gradientLayer = CAGradientLayer()
+    
+    @IBOutlet weak var checkingSavingsSegmentedControl: SegmentedControl!
+    @IBOutlet weak var nameTextField: FloatLabelTextField!
+    @IBOutlet weak var routingNumberTextField: FloatLabelTextField!
+    @IBOutlet weak var routingNumberTooltipButton: UIButton!
+    @IBOutlet weak var accountNumberTextField: FloatLabelTextField!
+    @IBOutlet weak var accountNumberTooltipButton: UIButton!
+    @IBOutlet weak var confirmAccountNumberTextField: FloatLabelTextField!
+    
+    let bag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +34,35 @@ class AutoPayViewController: UIViewController {
         let submitButton = UIBarButtonItem(title: NSLocalizedString("Submit", comment: ""), style: .done, target: self, action: #selector(onSubmitPress))
         navigationItem.leftBarButtonItem = cancelButton
         navigationItem.rightBarButtonItem = submitButton
+        
+        checkingSavingsSegmentedControl.items = [NSLocalizedString("Checking", comment: ""), NSLocalizedString("Savings", comment: "")]
+        
+        nameTextField.textField.placeholder = NSLocalizedString("Name on Account*", comment: "")
+        nameTextField.textField.delegate = self
+        nameTextField.textField.returnKeyType = .next
+        
+        routingNumberTextField.textField.placeholder = NSLocalizedString("Routing Number*", comment: "")
+        routingNumberTextField.textField.delegate = self
+        routingNumberTextField.textField.returnKeyType = .next
+        routingNumberTextField.textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        routingNumberTooltipButton.accessibilityLabel = NSLocalizedString("Tool tip", comment: "")
+        
+        accountNumberTextField.textField.placeholder = NSLocalizedString("Account Number*", comment: "")
+        accountNumberTextField.textField.delegate = self
+        accountNumberTextField.textField.returnKeyType = .next
+        accountNumberTextField.textField.isShowingAccessory = true
+        accountNumberTooltipButton.accessibilityLabel = NSLocalizedString("Tool tip", comment: "")
+        
+        confirmAccountNumberTextField.textField.placeholder = NSLocalizedString("Confirm Account Number*", comment: "")
+        confirmAccountNumberTextField.textField.delegate = self
+        confirmAccountNumberTextField.textField.returnKeyType = .next
+        
+        scrollView.rx.contentOffset.asDriver()
+            .map { $0.y <= 0 }
+            .distinctUntilChanged()
+            .map { $0 ? UIColor.softGray:UIColor.white }
+            .drive(onNext: { [weak self] color in self?.view.backgroundColor = color })
+            .addDisposableTo(bag)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -27,6 +71,23 @@ class AutoPayViewController: UIViewController {
         if let navController = navigationController as? MainBaseNavigationController {
             navController.setColoredNavBar()
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        gradientLayer.removeFromSuperlayer()
+        
+        let gLayer = CAGradientLayer()
+        gLayer.frame = gradientView.frame
+        gLayer.colors = [UIColor.softGray.cgColor, UIColor.white.cgColor]
+        
+        gradientLayer = gLayer
+        gradientView.layer.addSublayer(gLayer)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        gradientLayer.frame = gradientView.frame
     }
     
     func onCancelPress() {
@@ -38,3 +99,36 @@ class AutoPayViewController: UIViewController {
     }
     
 }
+
+extension AutoPayViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let newString = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+        let characterSet = CharacterSet(charactersIn: string)
+        if textField == routingNumberTextField.textField {
+            return CharacterSet.decimalDigits.isSuperset(of: characterSet) && newString.characters.count <= 9
+        } else if textField == accountNumberTextField.textField || textField == confirmAccountNumberTextField.textField {
+            return CharacterSet.decimalDigits.isSuperset(of: characterSet)
+        }
+        return true
+    }
+    
+    func textFieldDidChange(_ textField: UITextField) {
+        if textField == routingNumberTextField.textField && textField.text?.characters.count == 9 {
+            accountNumberTextField.textField.becomeFirstResponder()
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == nameTextField.textField {
+            routingNumberTextField.textField.becomeFirstResponder()
+        } else if textField == routingNumberTextField.textField {
+            accountNumberTextField.textField.becomeFirstResponder()
+        } else if textField == accountNumberTextField.textField {
+            if confirmAccountNumberTextField.isUserInteractionEnabled {
+                confirmAccountNumberTextField.textField.becomeFirstResponder()
+            }
+        }
+        return false
+    }
+}
+
