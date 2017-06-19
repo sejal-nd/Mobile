31f394405e53c64437f93e5f803342b33027040a
Marc Shilling <mshilling@mindgrub.com>
2017-06-19 18:04:28 -0400

    first stab at UI

--- Mobile/View Controllers/Bill/AutoPay/BGEAutoPayViewController.swift
+++ Mobile/View Controllers/Bill/AutoPay/BGEAutoPayViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 class BGEAutoPayViewController: UIViewController {
+    
+    @IBOutlet weak var gradientView: UIView!
+    var gradientLayer = CAGradientLayer()
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var stickyBottomView: UIView!
+    @IBOutlet weak var stickyBottomLabel: UILabel!
+    @IBOutlet weak var stickyBottomViewHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var learnMoreButton: ButtonControl!
+    @IBOutlet weak var learnMoreButtonLabel: UILabel!
+    
+    @IBOutlet weak var selectBankAccountLabel: UILabel!
+    
+    @IBOutlet weak var bankAccountButton: ButtonControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +35,33 @@ class BGEAutoPayViewController: UIViewController {
         let submitButton = UIBarButtonItem(title: NSLocalizedString("Submit", comment: ""), style: .done, target: self, action: #selector(onSubmitPress))
         navigationItem.leftBarButtonItem = cancelButton
         navigationItem.rightBarButtonItem = submitButton
+        
+        gradientLayer.frame = gradientView.bounds
+        gradientLayer.colors = [
+            UIColor.softGray.cgColor,
+            UIColor.white.cgColor,
+        ]
+        gradientView.layer.insertSublayer(gradientLayer, at: 0)
+        
+        learnMoreButton.backgroundColorOnPress = .softGray
+        learnMoreButton.addShadow(color: .black, opacity: 0.12, offset: CGSize(width: 0, height: 0), radius: 3)
+        learnMoreButtonLabel.textColor = .blackText
+        learnMoreButtonLabel.font = OpenSans.regular.of(size: 18)
+        let autoPayString = NSLocalizedString("AutoPay", comment: "")
+        let attrString = NSMutableAttributedString(string: String(format: NSLocalizedString("Learn more about\n%@", comment: ""), autoPayString))
+        attrString.addAttribute(NSFontAttributeName, value: OpenSans.bold.of(size: 18), range: (attrString.string as NSString).range(of: autoPayString))
+        learnMoreButtonLabel.attributedText = attrString
+        
+        selectBankAccountLabel.textColor = .blackText
+        selectBankAccountLabel.font = OpenSans.semibold.of(textStyle: .headline)
+        selectBankAccountLabel.text = NSLocalizedString("Select a bank account to enroll in AutoPay.", comment: "")
+        
+        bankAccountButton.backgroundColorOnPress = .softGray
+        bankAccountButton.addShadow(color: .black, opacity: 0.2, offset: CGSize(width: 0, height: 0), radius: 3)
+        
+        stickyBottomView.backgroundColor = .softGray
+        stickyBottomLabel.textColor = .blackText
+        stickyBottomLabel.text = NSLocalizedString("Your recurring payment will apply to the next BGE bill you receive. You will need to submit a payment for your current BGE bill if you have not already done so.", comment: "")
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -29,12 +72,22 @@ class BGEAutoPayViewController: UIViewController {
         }
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        gradientLayer.frame = gradientView.frame
+    }
+    
     func onCancelPress() {
         navigationController?.popViewController(animated: true)
     }
     
     func onSubmitPress() {
         print("Submit")
+    }
+    
+    @IBAction func onLearnMorePress() {
+        
     }
 
     @IBAction func onSelectBankAccountPress() {
