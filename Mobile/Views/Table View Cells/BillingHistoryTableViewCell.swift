72a3dfb9ee3e9c778dae4db914c4aae8e666c9e7
Jeremy Kliphouse <jkliphouse@mindgrub.com>
2017-06-22 16:12:44 -0400

    added the scheduled icon stubbed the VC and VM

--- Mobile/Views/Table View Cells/BillingHistoryTableViewCell.swift
+++ Mobile/Views/Table View Cells/BillingHistoryTableViewCell.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class BillingHistoryTableViewCell: UITableViewCell {
+    
+    let PAYMENT = "Payment"
+    let SCHEDULED_PAYMENT = "Scheduled Payment"
+    let BILL_ISSUED = "Bill Issued"
 
     @IBOutlet var iconImageView: UIImageView!
     @IBOutlet var titleLabel: UILabel!
@@ -19,6 +23,23 @@ class BillingHistoryTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    //TODO: this is not complete - just trying to get something working so I can test
+    //the webservices
+    func configureWith(amount: String, date: String, isFuture: Bool) {
+        self.amountLabel.text = amount
+        self.dateLabel.text = date
+        
+        if isFuture {
+            iconImageView.image = UIImage(named: "ic_scheduled")
+            titleLabel.text = SCHEDULED_PAYMENT
+        } else {
+            iconImageView.image = UIImage(named: "ic_bill")
+            titleLabel.text = BILL_ISSUED
+        }
+        
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
