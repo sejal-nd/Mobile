5a3132bf889ae5dd8b6615fb385d9699551f39cc
Constantin Koehler <constantinfakoehler@gmail.com>
2017-07-03 15:57:45 -0400

    Added implementation for anon change PW

--- Mobile/View Models/Login/LoginViewModel.swift
+++ Mobile/View Models/Login/LoginViewModel.swift
@@ -53,6 +53,11 @@ class LoginViewModel {
             .asObservable()
             .subscribe(onNext: { (profileStatus: ProfileStatus) in
                 onSuccess(profileStatus.tempPassword)
+                self.authService.logout().subscribe(onNext: {
+                    
+                }, onError: { (error) in
+                    print("Logout Error: \(error)")
+                }).addDisposableTo(self.disposeBag)
             }, onError: { error in
                 let serviceError = error as! ServiceError
                 if serviceError.serviceCode == ServiceErrorCode.FnAccountProtected.rawValue {
