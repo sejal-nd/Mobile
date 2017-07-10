5bb999cf319c92a647459268b67bffab716cfd7f
Jeremy Kliphouse <jkliphouse@mindgrub.com>
2017-07-10 17:08:57 -0400

    updated toasts to match confluence

--- Mobile/View Controllers/Bill/Wallet/EditCreditCardViewController.swift
+++ Mobile/View Controllers/Bill/Wallet/EditCreditCardViewController.swift
@@ -469,7 +469,7 @@ class EditCreditCardViewController: UIViewController {
                     self.oneTouchPayService.deleteTouchPayItem(forCustomerNumber: customerNumber)
                 }
                 LoadingView.hide()
-                self.delegate?.editCreditCardViewControllerDidEditAccount(self, message: NSLocalizedString("Card deleted", comment: ""))
+                self.delegate?.editCreditCardViewControllerDidEditAccount(self, message: NSLocalizedString("Card/Bank Account deleted", comment: ""))
                 _ = self.navigationController?.popViewController(animated: true)
             }, onError: { errMessage in
                 LoadingView.hide()
