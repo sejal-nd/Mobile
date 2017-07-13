229a24e95fd597ac91cf9afde2390cfc7948fd24
Junze Liu <jliu@mindgrub.com>
2017-07-13 15:19:44 -0400

    Added the unauthorized user experience entry screen. Linked Contact us and terms and policies screen to the entry screen. Link the log in screen in there too. Also test with tablet to confirm it looks good on that too.

---
+++ Mobile/View Controllers/Login/UnauthenticatedUserViewController.swift
@@ -0,0 +1,73 @@
+//
+//  UnauthenticatedUserViewController.swift
+//  Mobile
+//
+//  Created by Junze Liu on 7/13/17.
+//  Copyright © 2017 Exelon Corporation. All rights reserved.
+//
+
+import Foundation
+import Lottie
+
+class UnauthenticatedUserViewController: UIViewController {
+    @IBOutlet weak var lottieView: UIView!
+    
+    @IBOutlet weak var reportAnOutageButton: DisclosureButton!
+    @IBOutlet weak var checkMyOutageStatusButton: DisclosureButton!
+    @IBOutlet weak var viewOutageMapButton: DisclosureButton!
+    
+    
+    @IBOutlet weak var contactUsButton: DisclosureButton!
+    @IBOutlet weak var TermPoliciesButton: DisclosureButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let animationView = LOTAnimationView(name: "UU_OTP_animation") {
+            animationView.frame = CGRect(x: 0, y: 0, width: 230, height: 180)
+            animationView.contentMode = .scaleAspectFill
+            animationView.loopAnimation = true
+            
+            // put the animation at the center top screen
+            var center = lottieView.center
+            center.x = self.view.center.x;
+            animationView.center = center;
+            
+            lottieView.addSubview(animationView)
+            
+            animationView.play()
+        }
+        
+        
+        // For release one, only contact us and term policies button will be shown,
+        // so, hide the other 3 buttons
+        reportAnOutageButton.isHidden = true
+        checkMyOutageStatusButton.isHidden = true
+        viewOutageMapButton.isHidden = true
+        
+        view.backgroundColor = .primaryColor
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.view.backgroundColor = .primaryColor // This prevents a black color from appearing during the transition between `isTranslucent = false` and `isTranslucent = true`
+        navigationController?.navigationBar.barTintColor = .primaryColor
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barStyle = .black // Needed for white status bar
+        navigationController?.navigationBar.tintColor = .white
+        
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    @IBAction func onContactUsPress(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "ContactUsSegue", sender: self)
+    }
+    
+    @IBAction func onTermPoliciesPress(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "TermPoliciesSegue", sender: self)
+    }
+    
+    @IBAction func onLoginRegisterPress(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "signInRegisterSegue", sender: self)
+    }
+}
