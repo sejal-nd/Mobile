ae6d3d07e879e2a19d754e324c02975897fc53bb
Dan Jorquera <djorquera@mindgrub.com>
2017-07-19 14:02:07 -0400

    Skeleton created for OpCo specific template card

---
+++ Mobile/View Models/TemplateCardViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  BillHomeCard.swift
+//  Mobile
+//
+//  Created by Dan Jorquera on 7/19/17.
+//  Copyright © 2017 Exelon Corporation. All rights reserved.
+//
+
+
+import RxSwift
+import RxCocoa
+
+class HomeBillCardViewModel {
+    
+}
---
+++ Mobile/Views/TemplateCardView.swift
@@ -0,0 +1,16 @@
+//
+//  BillHomeCard.swift
+//  Mobile
+//
+//  Created by Dan Jorquera on 7/19/17.
+//  Copyright © 2017 Exelon Corporation. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import RxSwiftExt
+
+class HomeBillCardView: UIView {
+    
+}
