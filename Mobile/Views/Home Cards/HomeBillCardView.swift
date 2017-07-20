1bb5c9331090406f7736a4ce96e5bc00eeed22b4
Sam Francis <sfrancis@mindgrub.com>
2017-07-20 17:38:42 -0400

    More view stubbing and asset additions

--- Mobile/Views/Home Cards/HomeBillCardView.swift
+++ Mobile/Views/Home Cards/HomeBillCardView.swift
@@ -14,13 +14,45 @@ import RxSwiftExt
 class HomeBillCardView: UIView {
     
     var bag = DisposeBag()
-    @IBOutlet weak var billNotReadyStack: UIStackView!
-    @IBOutlet weak var errorStack: UIStackView!
+    
     @IBOutlet weak var infoStack: UIStackView!
+    @IBOutlet weak var alertImageView: UIImageView!
+    @IBOutlet weak var paymentPendingImageView: UIImageView!
+    @IBOutlet weak var paymentConfirmationImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var amountPaidLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
     
     @IBOutlet weak var dueDateStack: UIStackView!
     @IBOutlet weak var dueDateLabel: UILabel!
     @IBOutlet weak var dueDateTooltip: UIButton!
+    
+    @IBOutlet weak var dueAmountAndDateStack: UIStackView!
+    @IBOutlet weak var dueAmountAndDateLabel: UILabel!
+    @IBOutlet weak var dueAmountAndDateTooltip: UIButton!
+    
+    @IBOutlet weak var bankCreditNumberButton: ButtonControl!
+    @IBOutlet weak var bankCreditCardImageView: UIImageView!
+    @IBOutlet weak var bankCreditCardNumberLabel: UILabel!
+    
+    @IBOutlet weak var saveAPaymentAccountButton: ButtonControl!
+    @IBOutlet weak var saveAPaymentAccountLabel: UILabel!
+    
+    @IBOutlet weak var minimumPaymentLabel: UILabel!
+    
+    @IBOutlet weak var billNotReadyStack: UIStackView!
+    @IBOutlet weak var errorStack: UIStackView!
+    
+    @IBOutlet weak var oneTouchSlider: OneTouchSlider!
+    
+    @IBOutlet weak var scheduledImageView: UIImageView!
+    @IBOutlet weak var autoPayImageView: UIImageView!
+    
+    @IBOutlet weak var automaticPaymentInfoButton: UIButton!
+    @IBOutlet weak var thankYouForSchedulingButton: UIButton!
+    @IBOutlet weak var oneTouchPayTCButton: UIButton!
+    
+    @IBOutlet weak var viewBillButton: UIButton!
     
     private var viewModel: HomeBillCardViewModel! {
         didSet {
@@ -29,19 +61,13 @@ class HomeBillCardView: UIView {
         }
     }
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        styleViews()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+    override func layoutSubviews() {
+        super.layoutSubviews()
         styleViews()
     }
     
     static func create(withViewModel viewModel: HomeBillCardViewModel) -> HomeBillCardView {
         let view = Bundle.main.loadViewFromNib() as HomeBillCardView
-        view.styleViews()
         view.viewModel = viewModel
         return view
     }
@@ -49,6 +75,14 @@ class HomeBillCardView: UIView {
     private func styleViews() {
         addShadow(color: .black, opacity: 0.2, offset: .zero, radius: 3)
         layer.cornerRadius = 2
+        bankCreditNumberButton.layer.borderColor = UIColor.accentGray.cgColor
+        bankCreditNumberButton.layer.borderWidth = 2
+        bankCreditNumberButton.layer.cornerRadius = 3
+        
+        saveAPaymentAccountButton.layer.borderColor = UIColor.accentGray.cgColor
+        saveAPaymentAccountButton.layer.borderWidth = 2
+        saveAPaymentAccountButton.layer.cornerRadius = 3
+        saveAPaymentAccountLabel.font = OpenSans.semibold.of(textStyle: .footnote)
     }
     
     private func bindViewModel() {
