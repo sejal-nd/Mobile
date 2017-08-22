f7cc8e4e2f3fc80bc8ae02904d39de2771155fac
Marc Shilling <mshilling@mindgrub.com>
2017-08-22 10:18:42 -0400

    text changes for EM-2851

--- Mobile/View Models/Bill/PaymentViewModel.swift
+++ Mobile/View Models/Bill/PaymentViewModel.swift
@@ -893,9 +893,7 @@ class PaymentViewModel {
         if Environment.sharedInstance.opco == .bge {
             return true
         } else {
-            if $1 {
-                return false
-            } else if $2 {
+            if $1 || $2 {
                 return true
             }
             return !$0
@@ -913,11 +911,7 @@ class PaymentViewModel {
                 return NSLocalizedString("We accept: VISA, MasterCard, Discover, and American Express. Business customers cannot use VISA.\n\nAny payment made for less than the total amount due or after the indicated due date may result in your service being disconnected. Payments may take up to two business days to reflect on your account.", comment: "")
             }
         } else {
-            if $1 {
-                return NSLocalizedString("We accept: Discover, MasterCard, and Visa Credit Cards or Check Cards, and ATM Debit Cards with a PULSE, STAR, NYCE, or ACCEL logo. American Express is not accepted at this time.", comment: "")
-            } else {
-                return NSLocalizedString("Up to three payment accounts for credit cards and bank accounts may be saved.\n\nWe accept: Discover, MasterCard, and Visa Credit Cards or Check Cards, and ATM Debit Cards with a PULSE, STAR, NYCE, or ACCEL logo. American Express is not accepted at this time.", comment: "")
-            }
+            return NSLocalizedString("Up to three payment accounts for credit cards and bank accounts may be saved.\n\nWe accept: Discover, MasterCard, and Visa Credit Cards or Check Cards, and ATM Debit Cards with a PULSE, STAR, NYCE, or ACCEL logo. American Express is not accepted at this time.", comment: "")
         }
     }
     
