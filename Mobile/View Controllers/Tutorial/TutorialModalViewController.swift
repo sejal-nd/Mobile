fddb1929e92b33f4a33c895d29297faa394673be
James Landrum <jlandrum@mindgrub.com>
2017-09-25 00:18:45 -0400

    configure next/got it button

--- Mobile/View Controllers/Tutorial/TutorialModalViewController.swift
+++ Mobile/View Controllers/Tutorial/TutorialModalViewController.swift
@@ -11,6 +11,9 @@ class TutorialModalViewController: DismissableFormSheetViewController {
     @IBOutlet weak var rootView: UIView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var xButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+
+    var slides = Array<TutorialSlide>()
     
     init() {
         super.init(nibName: "TutorialModal", bundle: nil)
@@ -20,43 +23,56 @@ class TutorialModalViewController: DismissableFormSheetViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         rootView.backgroundColor = .primaryColor
         scrollView.delegate = self
         pageControl.pageIndicatorTintColor = .primaryColorDark
+
+        slides.forEach {
+            addViewFor(slide: $0)
+        }
+    }
+    
+    func setCurrentPage(_ page: Int) {
+        pageControl.currentPage = page
+        let lastPage = scrollView.currentPage == pageControl.numberOfPages - 1
+        nextButton.setTitle(lastPage ? NSLocalizedString("Got It!", comment: "") : NSLocalizedString("Next", comment: ""), for: .normal)
+    }
+    
+    @IBAction func onNext(_ sender: Any) {
+        let pageCount = pageControl.numberOfPages
+        let page = scrollView.currentPage
+        
+        if (page < pageCount-1) {
+            UIView.animate(withDuration: 0.3) {
+                self.scrollView.contentOffset.x = self.scrollView.frame.width
+                * CGFloat(page+1)
+            }
+            setCurrentPage(page+1)
+        } else {
+            onClose(sender)
+        }
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        addSlide(title: "Set Up Default Payment Account",
-                 text: "You can easily pay your bill in full from the Home " +
-            "screen by setting a payment account as default.",
-                 animation: "otp_step1")
-        addSlide(title: "Tap On My Wallet",
-                 text: "Navigate to the Bill screen and tap \"My Wallet.\" " +
-                    "You can also tap the \"Set a default payment account\" button " +
-            "on Home.",
-                 animation: "otp_step2")
-        addSlide(title: "Turn On The Default Toggle",
-                 text: "Create or edit a payment account and turn on the " +
-            "\"Default Payment Account\" toggle.",
-                 animation: "otp_step3")
-        addSlide(title: "Pay From The Home Screen!",
-                 text: "You can now easily pay from the Home screen. This " +
-                    "type of payment cannot be canceled and will pay your account " +
-            "balance in full.",
-                 animation: "otp_step4")
     }
 
     func addSlide(title:String,
-                 text:String,
-                 animation:String) {
-        let viewCopy = TutorialView(frame: scrollView.frame,
-                                    title: title,
+                      text:String,
+                      animation:String) {
+        slides.append(TutorialSlide(title: title,
                                     message: text,
-                                    animation: animation)
+                                    animation: animation))
+    }
+    
+    func addViewFor(slide:TutorialSlide) {
+        let viewCopy = TutorialView(frame: scrollView.frame,
+                                    title: slide.title,
+                                    message: slide.message,
+                                    animation: slide.animation)
         pagerContent.addArrangedSubview(viewCopy)
         pageControl.numberOfPages = pagerContent.subviews.count
         viewCopy.view.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
@@ -69,12 +85,29 @@ class TutorialModalViewController: DismissableFormSheetViewController {
     }
 }
 
+class TutorialSlide {
+    let title: String
+    let message: String
+    let animation: String
+    
+    init(title:String, message:String, animation: String) {
+        self.title = title
+        self.message = message
+        self.animation = animation
+    }
+}
+
 extension TutorialModalViewController: UIScrollViewDelegate {
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let currentPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
-        
-        pageControl.currentPage = currentPage
+        setCurrentPage(scrollView.currentPage)
+        pageControl.currentPage = scrollView.currentPage
     }
-    
+}
+
+extension UIScrollView {
+    var currentPage: Int {
+        get { return Int(self.contentOffset.x) / Int(self.frame.width) }
+        set {}
+    }
 }
 
