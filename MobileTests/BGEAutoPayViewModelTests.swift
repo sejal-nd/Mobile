f888d524b11aef25c5bc135eb9c7029178e22fb0
Marc Shilling <marcshilling@gmail.com>
2017-09-29 15:47:35 -0400

    Add more BGEAutoPayViewModel tests

--- MobileTests/BGEAutoPayViewModelTests.swift
+++ MobileTests/BGEAutoPayViewModelTests.swift
@@ -14,10 +14,65 @@ class BGEAutoPayViewModelTests: XCTestCase {
     var viewModel: BGEAutoPayViewModel!
     let disposeBag = DisposeBag()
     
-    func testIsUnenrolling() {
-        let accountDetail = AccountDetail.from(["accountNumber": "0123456789", "isBudgetBill": true, "CustomerInfo": [:], "BillingInfo": [:]])!
+    func testShowBottomLabel() {
+        var accountDetail = AccountDetail.from(["accountNumber": "0123456789", "isAutoPay": false, "CustomerInfo": [:], "BillingInfo": [:]])!
         viewModel = BGEAutoPayViewModel(paymentService: ServiceFactory.createPaymentService(), accountDetail: accountDetail)
-        viewModel.initialEnrollmentStatus.value = BGEAutoPayViewModel.EnrollmentStatus.enrolled
+        viewModel.showBottomLabel.asObservable().take(1).subscribe(onNext: { show in
+            if !show {
+                XCTFail("Bottom label should show when not enrolled")
+            }
+        }).disposed(by: disposeBag)
+        
+        accountDetail = AccountDetail.from(["accountNumber": "0123456789", "isAutoPay": true, "CustomerInfo": [:], "BillingInfo": [:]])!
+        viewModel = BGEAutoPayViewModel(paymentService: ServiceFactory.createPaymentService(), accountDetail: accountDetail)
+        viewModel.showBottomLabel.asObservable().take(1).subscribe(onNext: { show in
+            if show {
+                XCTFail("Bottom label should not show when enrolled")
+            }
+        }).disposed(by: disposeBag)
+    }
+    
+    func testSubmitButtonEnabled() {
+        var accountDetail = AccountDetail.from(["accountNumber": "0123456789", "isAutoPay": false, "CustomerInfo": [:], "BillingInfo": [:]])!
+        viewModel = BGEAutoPayViewModel(paymentService: ServiceFactory.createPaymentService(), accountDetail: accountDetail)
+        viewModel.selectedWalletItem.value = WalletItem()
+        viewModel.submitButtonEnabled.asObservable().take(1).subscribe(onNext: { enabled in
+            if !enabled {
+                XCTFail("Submit button should be enabled when unenrolled with a selected bank account")
+            }
+        }).disposed(by: disposeBag)
+        
+        accountDetail = AccountDetail.from(["accountNumber": "0123456789", "isAutoPay": true, "CustomerInfo": [:], "BillingInfo": [:]])!
+        viewModel = BGEAutoPayViewModel(paymentService: ServiceFactory.createPaymentService(), accountDetail: accountDetail)
+        viewModel.enrollSwitchValue.value = false
+        viewModel.submitButtonEnabled.asObservable().take(1).subscribe(onNext: { enabled in
+            if !enabled {
+                XCTFail("Submit button should be enabled when enrolled and switch toggled off")
+            }
+        }).disposed(by: disposeBag)
+        
+        accountDetail = AccountDetail.from(["accountNumber": "0123456789", "isAutoPay": true, "CustomerInfo": [:], "BillingInfo": [:]])!
+        viewModel = BGEAutoPayViewModel(paymentService: ServiceFactory.createPaymentService(), accountDetail: accountDetail)
+        viewModel.selectedWalletItem.value = WalletItem()
+        viewModel.userDidChangeSettings.value = true
+        viewModel.submitButtonEnabled.asObservable().take(1).subscribe(onNext: { enabled in
+            if !enabled {
+                XCTFail("Submit button should be enabled when enrolled and settings changed")
+            }
+        }).disposed(by: disposeBag)
+        
+        viewModel.userDidChangeSettings.value = false
+        viewModel.userDidChangeBankAccount.value = true
+        viewModel.submitButtonEnabled.asObservable().take(1).subscribe(onNext: { enabled in
+            if !enabled {
+                XCTFail("Submit button should be enabled when enrolled and bank account changed")
+            }
+        }).disposed(by: disposeBag)
+    }
+    
+    func testIsUnenrolling() {
+        let accountDetail = AccountDetail.from(["accountNumber": "0123456789", "isAutoPay": true, "CustomerInfo": [:], "BillingInfo": [:]])!
+        viewModel = BGEAutoPayViewModel(paymentService: ServiceFactory.createPaymentService(), accountDetail: accountDetail)
         viewModel.enrollSwitchValue.value = false
         viewModel.isUnenrolling.asObservable().take(1).subscribe(onNext: { isUnenrolling in
             if !isUnenrolling {
@@ -25,5 +80,28 @@ class BGEAutoPayViewModelTests: XCTestCase {
             }
         }).disposed(by: disposeBag)
     }
-
+    
+    func testShouldShowSettingsButton() {
+        var accountDetail = AccountDetail.from(["accountNumber": "0123456789", "isAutoPay": true, "CustomerInfo": [:], "BillingInfo": [:]])!
+        viewModel = BGEAutoPayViewModel(paymentService: ServiceFactory.createPaymentService(), accountDetail: accountDetail)
+        viewModel.shouldShowSettingsButton.asObservable().take(1).subscribe(onNext: { show in
+            if !show {
+                XCTFail("Settings button should always show when enrolled")
+            }
+        }).disposed(by: disposeBag)
+        
+        accountDetail = AccountDetail.from(["accountNumber": "0123456789", "isAutoPay": false, "CustomerInfo": [:], "BillingInfo": [:]])!
+        viewModel = BGEAutoPayViewModel(paymentService: ServiceFactory.createPaymentService(), accountDetail: accountDetail)
+        viewModel.selectedWalletItem.value = WalletItem()
+        viewModel.shouldShowSettingsButton.asObservable().take(1).subscribe(onNext: { show in
+            if !show {
+                XCTFail("Settings button should show when unenrolled with a selected bank account")
+            }
+        }).disposed(by: disposeBag)
+        
+        // Note: Case where settings button should be hidden when unenrolling is covered by
+        // testIsUnenrolling. Entire stack view that contains the settings button is hidden
+        // when unenrolling
+    }
+    
 }
