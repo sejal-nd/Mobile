6403a43fadc2e574aad779d73f0cc70f89bdb2ef
Marc Shilling <marcshilling@gmail.com>
2017-10-02 15:59:10 -0400

    setup AutoPayViewModelTests

---
+++ MobileTests/AutoPayViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  AutoPayViewModelTests.swift
+//  MobileTests
+//
+//  Created by Marc Shilling on 10/2/17.
+//  Copyright © 2017 Exelon Corporation. All rights reserved.
+//
+
+import XCTest
+import RxSwift
+
+class AutoPayViewModelTests: XCTestCase {
+    
+    var viewModel: AutoPayViewModel!
+    let disposeBag = DisposeBag()
+}
