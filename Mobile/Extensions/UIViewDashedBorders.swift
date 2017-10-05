a671d174713c805049e730e2da29665a1c13a13b
Marc Shilling <marcshilling@gmail.com>
2017-10-05 16:02:05 -0400

    all the graph bars laid out

---
+++ Mobile/Extensions/UIViewDashedBorders.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewDashedBorders.swift
+//  Mobile
+//
+//  Created by Marc Shilling on 10/5/17.
+//  Copyright © 2017 Exelon Corporation. All rights reserved.
+//
+
+extension UIView {
+    func addDashedBorder(color: UIColor) {
+        let borderColor = color.cgColor
+        
+        let shapeLayer:CAShapeLayer = CAShapeLayer()
+        let frameSize = self.frame.size
+        let shapeRect = CGRect(x: 0, y: 0, width: frameSize.width, height: frameSize.height)
+        
+        shapeLayer.bounds = shapeRect
+        shapeLayer.position = CGPoint(x: frameSize.width / 2, y: frameSize.height / 2)
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = borderColor
+        shapeLayer.lineWidth = 1
+        shapeLayer.lineJoin = kCALineJoinRound
+        shapeLayer.lineDashPattern = [4, 1]
+        shapeLayer.path = UIBezierPath(roundedRect: shapeRect, cornerRadius: 0).cgPath
+        
+        self.layer.addSublayer(shapeLayer)
+    }
+}
--- Mobile/View Controllers/Bill/Bill Analysis/BillAnalysisViewController.swift
+++ Mobile/View Controllers/Bill/Bill Analysis/BillAnalysisViewController.swift
@@ -11,9 +11,39 @@ import UIKit
 class BillAnalysisViewController: UIViewController {
     
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var stackViewWidthConstraint: NSLayoutConstraint! // So that we can design every bar in IB but set it to 460 in viewDidLoad
     
     @IBOutlet weak var electricGasSegmentView: UIView!
     @IBOutlet weak var electricGasSegmentedControl: SegmentedControl!
+    
+    @IBOutlet weak var billComparisonSegmentedControl: BillAnalysisSegmentedControl!
+    
+    @IBOutlet weak var noDataContainerButton: ButtonControl!
+    @IBOutlet weak var noDataBarView: UIView!
+    @IBOutlet weak var noDataLabel: UILabel!
+    @IBOutlet weak var noDataDateLabel: UILabel!
+
+    @IBOutlet weak var previousContainerButton: ButtonControl!
+    @IBOutlet weak var previousDollarLabel: UILabel!
+    @IBOutlet weak var previousBarView: UIView!
+    @IBOutlet weak var previousDateLabel: UILabel!
+    
+    @IBOutlet weak var currentContainerButton: ButtonControl!
+    @IBOutlet weak var currentDollarLabel: UILabel!
+    @IBOutlet weak var currentBarView: UIView!
+    @IBOutlet weak var currentDateLabel: UILabel!
+    
+    @IBOutlet weak var projectedContainerButton: ButtonControl!
+    @IBOutlet weak var projectedDollarLabel: UILabel!
+    @IBOutlet weak var projectedBarImage: UIImageView!
+    @IBOutlet weak var projectedDateLabel: UILabel!
+    
+    @IBOutlet weak var projectionNotAvailableContainerButton: ButtonControl!
+    @IBOutlet weak var projectionNotAvailableBarView: UIView!
+    @IBOutlet weak var projectionNotAvailableDaysRemainingLabel: UILabel!
+    @IBOutlet weak var projectionNotAvailableUntilNextForecastLabel: UILabel!
+    @IBOutlet weak var projectionNotAvailableDateLabel: UILabel!
     
     @IBOutlet weak var barDescriptionView: UIView!
     @IBOutlet weak var barDescriptionDateLabel: UILabel!
@@ -33,8 +63,17 @@ class BillAnalysisViewController: UIViewController {
         
         title = NSLocalizedString("Bill Analysis", comment: "")
         
+        billComparisonSegmentedControl.setItems(leftLabel: NSLocalizedString("Last Year", comment: ""), rightLabel: NSLocalizedString("Previous Bill", comment: ""), initialSelectedIndex: 1)
+        
         styleViews()
         
+        previousContainerButton.isHidden = true
+        currentContainerButton.isHidden = true
+        stackViewWidthConstraint.constant = 460
+        
+        if UIScreen.main.bounds.size.width < 375 { // If smaller than iPhone 6 width
+            stackView.spacing = 11
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -49,6 +88,18 @@ class BillAnalysisViewController: UIViewController {
         scrollView.backgroundColor = .softGray
         
         electricGasSegmentedControl.items = [NSLocalizedString("Electric", comment: ""), NSLocalizedString("Gas", comment: "")]
+        
+        noDataBarView.addDashedBorder(color: UIColor(red: 0, green: 80/255, blue: 125/255, alpha: 0.24))
+        projectionNotAvailableBarView.addDashedBorder(color: UIColor(red: 0, green: 80/255, blue: 125/255, alpha: 0.24))
+        
+        switch Environment.sharedInstance.opco {
+        case .bge:
+            projectedBarImage.tintColor = UIColor(red: 0, green: 110/255, blue: 187/255, alpha: 1)
+        case .comEd:
+            projectedBarImage.tintColor = UIColor(red: 0, green: 145/255, blue: 182/255, alpha: 1)
+        case .peco:
+            projectedBarImage.tintColor = UIColor(red: 114/255, green: 184/255, blue: 101/255, alpha: 1)
+        }
         
         barDescriptionView.addShadow(color: .black, opacity: 0.08, offset: .zero, radius: 2)
         barDescriptionDateLabel.font = OpenSans.semibold.of(textStyle: .subheadline)
