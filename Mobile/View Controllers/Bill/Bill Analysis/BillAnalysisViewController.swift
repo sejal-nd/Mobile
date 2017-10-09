b4767f2a30dbccefd5385e68b60d7b19b5635dd0
Marc Shilling <marcshilling@gmail.com>
2017-10-09 11:21:15 -0400

    pie chart and legend styling complete

--- Mobile/View Controllers/Bill/Bill Analysis/BillAnalysisViewController.swift
+++ Mobile/View Controllers/Bill/Bill Analysis/BillAnalysisViewController.swift
@@ -23,7 +23,20 @@ class BillAnalysisViewController: UIViewController {
     @IBOutlet weak var currentChargesSummaryView: UIView!
     @IBOutlet weak var currentChargesSummaryLabel: UILabel!
     @IBOutlet weak var currentChargesPieChartView: PieChartView!
-    @IBOutlet weak var currentChargesBreakdownView: UIView!
+    
+    @IBOutlet weak var currentChargesLegendView: UIView!
+    @IBOutlet weak var supplyLegendBox: UIView!
+    @IBOutlet weak var supplyLegendLabel: UILabel!
+    @IBOutlet weak var supplyValueLabel: UILabel!
+    @IBOutlet weak var taxesFeesLegendBox: UIView!
+    @IBOutlet weak var taxesFeesLegendLabel: UILabel!
+    @IBOutlet weak var taxesFeesValueLabel: UILabel!
+    @IBOutlet weak var deliveryLegendBox: UIView!
+    @IBOutlet weak var deliveryLegendLabel: UILabel!
+    @IBOutlet weak var deliveryValueLabel: UILabel!
+    @IBOutlet weak var totalChargesLegendBox: UIView!
+    @IBOutlet weak var totalChargesLegendLabel: UILabel!
+    @IBOutlet weak var totalChargesValueLabel: UILabel!
     
     // Bill Comparison
     @IBOutlet weak var billComparisonTitleLabel: UILabel!
@@ -92,6 +105,7 @@ class BillAnalysisViewController: UIViewController {
     
     @IBOutlet weak var footerLabel: UILabel!
     
+    let centerPieChartColor = UIColor(red: 89/255, green: 103/255, blue: 113/255, alpha: 1)
     
     let viewModel = BillAnalysisViewModel(usageService: ServiceFactory.createUsageService())
     
@@ -178,7 +192,35 @@ class BillAnalysisViewController: UIViewController {
     }
     
     private func styleCurrentChargesBreakdownView() {
-        currentChargesBreakdownView.addShadow(color: .black, opacity: 0.08, offset: .zero, radius: 2)
+        currentChargesLegendView.addShadow(color: .black, opacity: 0.08, offset: .zero, radius: 2)
+        
+        supplyLegendBox.backgroundColor = .accentGray
+        supplyLegendLabel.textColor = .blackText
+        supplyLegendLabel.font = OpenSans.semibold.of(textStyle: .headline)
+        supplyLegendLabel.text = NSLocalizedString("Supply", comment: "")
+        supplyValueLabel.textColor = .blackText
+        supplyValueLabel.font = OpenSans.regular.of(textStyle: .subheadline)
+        
+        taxesFeesLegendBox.backgroundColor = .blackText
+        taxesFeesLegendLabel.textColor = .blackText
+        taxesFeesLegendLabel.font = OpenSans.semibold.of(textStyle: .headline)
+        taxesFeesLegendLabel.text = NSLocalizedString("Taxes & Fees", comment: "")
+        taxesFeesValueLabel.textColor = .blackText
+        taxesFeesValueLabel.font = OpenSans.regular.of(textStyle: .subheadline)
+        
+        deliveryLegendBox.backgroundColor = .primaryColor
+        deliveryLegendLabel.textColor = .blackText
+        deliveryLegendLabel.font = OpenSans.semibold.of(textStyle: .headline)
+        deliveryLegendLabel.text = NSLocalizedString("Delivery", comment: "")
+        deliveryValueLabel.textColor = .blackText
+        deliveryValueLabel.font = OpenSans.regular.of(textStyle: .subheadline)
+        
+        totalChargesLegendBox.backgroundColor = centerPieChartColor
+        totalChargesLegendLabel.textColor = .deepGray
+        totalChargesLegendLabel.font = OpenSans.bold.of(textStyle: .headline)
+        totalChargesLegendLabel.text = NSLocalizedString("TOTAL CHARGES", comment: "")
+        totalChargesValueLabel.textColor = .deepGray
+        totalChargesValueLabel.font = OpenSans.bold.of(textStyle: .subheadline)
     }
     
     private func stylePieChart() {
@@ -189,14 +231,15 @@ class BillAnalysisViewController: UIViewController {
         dataSet.colors = [.accentGray, .blackText, .primaryColor]
         dataSet.drawValuesEnabled = false
         dataSet.highlightEnabled = false // Disable selection
+        dataSet.sliceSpace = 4
         let data = PieChartData(dataSet: dataSet)
         currentChargesPieChartView.data = data
         
         currentChargesPieChartView.legend.enabled = false // Hide the legend because we'll draw our own
         //currentChargesPieChartView.drawSlicesUnderHoleEnabled = false
         currentChargesPieChartView.chartDescription?.enabled = false // Hides the chart description
-        currentChargesPieChartView.holeColor = UIColor(red: 89/255, green: 103/255, blue: 113/255, alpha: 1)
-        currentChargesPieChartView.holeRadiusPercent = 0.70
+        currentChargesPieChartView.holeColor = centerPieChartColor
+        currentChargesPieChartView.holeRadiusPercent = 0.71
         currentChargesPieChartView.transparentCircleRadiusPercent = 0.75
         currentChargesPieChartView.transparentCircleColor = .softGray
 
