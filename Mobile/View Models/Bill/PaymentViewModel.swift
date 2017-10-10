9baf66df7e245c80ae6418881c39b9e265baca4f
Sam Francis <sfrancis@mindgrub.com>
2017-10-10 14:02:37 -0400

    Time zone fix for the calendar picker.

--- Mobile/View Models/Bill/PaymentViewModel.swift
+++ Mobile/View Models/Bill/PaymentViewModel.swift
@@ -70,17 +70,18 @@ class PaymentViewModel {
         
         let now = Date()
         self.paymentDate = Variable(now)
+        let startOfTodayDate = Calendar.current.startOfDay(for: now)
+        let tomorrow =  Calendar.current.date(byAdding: .day, value: 1, to: startOfTodayDate)!
+        
         if Environment.sharedInstance.opco == .bge &&
             Calendar.opCoTime.component(.hour, from: Date()) >= 20 &&
             !accountDetail.isActiveSeverance {
-            let startOfTodayDate = Calendar.opCoTime.startOfDay(for: now)
-            let tomorrow =  Calendar.opCoTime.date(byAdding: .day, value: 1, to: startOfTodayDate)!
             self.paymentDate.value = tomorrow
         }
         if Environment.sharedInstance.opco == .bge &&
-            Calendar.opCoTime.component(.hour, from: Date()) < 20 &&
-            !accountDetail.isActiveSeverance && !self.fixedPaymentDateLogic(accountDetail: accountDetail, cardWorkflow: false, inlineCard: false, saveBank: true, saveCard: true, allowEdits: allowEdits.value) {
-            self.paymentDate.value = now
+            !accountDetail.isActiveSeverance &&
+            !self.fixedPaymentDateLogic(accountDetail: accountDetail, cardWorkflow: false, inlineCard: false, saveBank: true, saveCard: true, allowEdits: allowEdits.value) {
+            self.paymentDate.value = Calendar.opCoTime.component(.hour, from: Date()) < 20 ? now: tomorrow
         } else if let dueDate = accountDetail.billingInfo.dueByDate {
             if dueDate >= now && !self.fixedPaymentDateLogic(accountDetail: accountDetail, cardWorkflow: false, inlineCard: false, saveBank: true, saveCard: true, allowEdits: allowEdits.value) {
                 self.paymentDate.value = dueDate
