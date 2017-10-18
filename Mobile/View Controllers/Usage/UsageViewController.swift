ca208c8bf76f35cbcecd1502e4d6cfb703690060
Sam Francis <sfrancis@mindgrub.com>
2017-10-18 17:27:07 -0400

    Stubbed out the usage screen with buttons and segues to other screens

--- Mobile/View Controllers/Usage/UsageViewController.swift
+++ Mobile/View Controllers/Usage/UsageViewController.swift
@@ -7,13 +7,26 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class UsageViewController: UIViewController {
+    
+    let disposeBag = DisposeBag()
 
+    @IBOutlet weak var accountPicker: AccountPicker!
+    @IBOutlet weak var usageGraphPlaceholderButton: DisclosureButton!
+    @IBOutlet weak var top5EnergyTipsButton: DisclosureButton!
+    @IBOutlet weak var updateYourHomeProfileButton: DisclosureButton!
+    @IBOutlet weak var takeMeToSavingsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        title = NSLocalizedString("Usage", comment: "")
+        
+        accountPicker.loadAccounts()
+        accountPicker.updateCurrentAccount()
+        
+        buttonTapSetup()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -23,6 +36,32 @@ class UsageViewController: UIViewController {
             navController.setWhiteNavBar()
         }
     }
+    
+    func buttonTapSetup() {
+        Driver.merge(usageGraphPlaceholderButton.rx.tap.asDriver().mapTo("usageWebViewSegue"),
+                     top5EnergyTipsButton.rx.tap.asDriver().mapTo("top5EnergyTipsSegue"),
+                     updateYourHomeProfileButton.rx.tap.asDriver().mapTo("updateYourHomeProfileSegue"),
+                     takeMeToSavingsButton.rx.tap.asDriver().mapTo("takeMeToSavingsSegue"))
+            .drive(onNext: { [weak self] in
+                self?.performSegue(withIdentifier: $0, sender: nil)
+            })
+            .disposed(by: disposeBag)
+    }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.destination {
+        //case let usageVC as UsageWebViewController:
+        case _ as UsageWebViewController:
+            break
+        case _ as Top5EnergyTipsViewController:
+            break
+        case _ as MyHomeProfileViewController:
+            break
+        case _ as TotalPeakTimeSavingsViewController:
+            break
+        default:
+            break
+        }
+    }
 
 }
---
+++ Mobile/View Models/Usage/UsageViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  UsageViewModel.swift
+//  Mobile
+//
+//  Created by Sam Francis on 10/18/17.
+//  Copyright © 2017 Exelon Corporation. All rights reserved.
+//
+
+import Foundation
+
+class UsageViewModel {
+    
+    
+}
