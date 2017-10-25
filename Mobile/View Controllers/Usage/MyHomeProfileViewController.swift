0f7a37192a9a77018d0b6a013cca525d7dca0826
Sam Francis <sfrancis@mindgrub.com>
2017-10-25 11:35:03 -0400

    Stubbed out the home profile UI

--- Mobile/View Controllers/Usage/MyHomeProfileViewController.swift
+++ Mobile/View Controllers/Usage/MyHomeProfileViewController.swift
@@ -10,9 +10,62 @@ import UIKit
 
 class MyHomeProfileViewController: UIViewController {
 
+    @IBOutlet weak var headerLabel: UILabel!
+    
+    @IBOutlet weak var homeTypeInfoLabel: UILabel!
+    @IBOutlet weak var homeTypeButton: DisclosureButton!
+    
+    @IBOutlet weak var heatingFuelInfoLabel: UILabel!
+    @IBOutlet weak var heatingFuelButton: DisclosureButton!
+    
+    @IBOutlet weak var numberOfResidentsInfoLabel: UILabel!
+    @IBOutlet weak var numberOfAdultsButton: DisclosureButton!
+    @IBOutlet weak var numberOfChildrenButton: DisclosureButton!
+    
+    @IBOutlet weak var homeSizeInfoLabel: UILabel!
+    @IBOutlet weak var homeSizeTextField: FloatLabelTextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: NSLocalizedString("Save", comment: ""),
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(savePressed))
+        styleViews()
+    }
+    
+    func styleViews() {
+        headerLabel.numberOfLines = 0
+        headerLabel.font = SystemFont.regular.of(textStyle: .headline)
+        headerLabel.setLineHeight(lineHeight: 24)
+        homeTypeInfoLabel.numberOfLines = 0
+        homeTypeInfoLabel.font = SystemFont.regular.of(textStyle: .subheadline)
+        heatingFuelInfoLabel.numberOfLines = 0
+        heatingFuelInfoLabel.font = SystemFont.regular.of(textStyle: .subheadline)
+        numberOfResidentsInfoLabel.numberOfLines = 0
+        numberOfResidentsInfoLabel.font = SystemFont.regular.of(textStyle: .subheadline)
+        homeSizeInfoLabel.numberOfLines = 0
+        homeSizeInfoLabel.font = SystemFont.regular.of(textStyle: .subheadline)
+        
+        homeSizeTextField.textField.placeholder = NSLocalizedString("Home Size (sq. ft)", comment: "")
+        homeSizeTextField.textField.delegate = self
+        homeSizeTextField.textField.returnKeyType = .done
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let navController = navigationController as? MainBaseNavigationController {
+            navController.setColoredNavBar()
+        }
     }
 
+    @objc func savePressed() {
+        dLog("Save Pressed")
+    }
+}
+
+extension MyHomeProfileViewController: UITextFieldDelegate {
+    
 }
