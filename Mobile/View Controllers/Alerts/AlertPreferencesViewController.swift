106ae2cf1eceae98c695f7c0ed4155da11b89704
Marc Shilling <marcshilling@gmail.com>
2017-11-03 13:32:55 -0400

    removed account picker, added account info slice

--- Mobile/View Controllers/Alerts/AlertPreferencesViewController.swift
+++ Mobile/View Controllers/Alerts/AlertPreferencesViewController.swift
@@ -8,12 +8,13 @@
 
 import RxSwift
 
-class AlertPreferencesViewController: AccountPickerViewController {
+class AlertPreferencesViewController: UIViewController {
     
     let disposeBag = DisposeBag()
     
-    @IBOutlet weak var accountPickerContainerView: UIView!
-    @IBOutlet weak var accountPickerSpacerView: UIView!
+    @IBOutlet weak var accountInfoBar: AccountInfoBar!
+    
+    let viewModel = AlertPreferencesViewModel()
     
     // Prevents status bar color flash when pushed
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -25,28 +26,9 @@ class AlertPreferencesViewController: AccountPickerViewController {
 
         title = NSLocalizedString("Alert Preferences", comment: "")
         
-        accountPickerContainerView.backgroundColor = .primaryColor
         if Environment.sharedInstance.opco == .bge {
-            accountPicker.isHidden = true
-            accountPickerSpacerView.isHidden = true
+            accountInfoBar.isHidden = true
         }
-        
-        accountPicker.delegate = self
-        accountPicker.parentViewController = self
-        accountPickerViewControllerWillAppear.subscribe(onNext: { [weak self] state in
-            guard let `self` = self else { return }
-            switch(state) {
-            case .loadingAccounts:
-                break
-            case .readyToFetchData:
-                print("Alert Preferences - Fetch Data")
-//                if AccountsStore.sharedInstance.currentAccount != self.accountPicker.currentAccount {
-//                    self.getOutageStatus()
-//                } else if self.viewModel.currentOutageStatus == nil {
-//                    self.getOutageStatus()
-//                }
-            }
-        }).disposed(by: disposeBag)
         
     }
     
@@ -54,18 +36,10 @@ class AlertPreferencesViewController: AccountPickerViewController {
         super.viewWillAppear(animated)
         
         if let navController = navigationController as? MainBaseNavigationController {
-            navController.setColoredNavBar(hidesBottomBorder: true)
+            navController.setColoredNavBar()
         }
     }
     
 
 
-}
-
-extension AlertPreferencesViewController: AccountPickerDelegate {
-    
-    func accountPickerDidChangeAccount(_ accountPicker: AccountPicker) {
-        print("Alert Preferences - Changed Account - Fetch Data")
-    }
-    
 }
