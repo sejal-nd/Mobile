844885af361483f0d07e93211b56b3b664f51beb
Marc Shilling <marcshilling@gmail.com>
2017-11-03 13:02:59 -0400

    all views in storyboard and IBOutlets created

--- Mobile/View Controllers/Alerts/AlertPreferencesViewController.swift
+++ Mobile/View Controllers/Alerts/AlertPreferencesViewController.swift
@@ -15,6 +15,37 @@ class AlertPreferencesViewController: AccountPickerViewController {
     @IBOutlet weak var accountPickerContainerView: UIView!
     @IBOutlet weak var accountPickerSpacerView: UIView!
     
+    @IBOutlet weak var outageTitleLabel: UILabel!
+    @IBOutlet weak var outageDetailLabel: UILabel!
+    @IBOutlet weak var outageSwitch: Switch!
+    
+    @IBOutlet weak var scheduledMaintView: UIView!
+    @IBOutlet weak var scheduledMaintTitleLabel: UILabel!
+    @IBOutlet weak var scheduledMaintDetailLabel: UILabel!
+    @IBOutlet weak var scheduledMaintSwitch: Switch!
+    
+    @IBOutlet weak var severeWeatherTitleLabel: UILabel!
+    @IBOutlet weak var severeWeatherDetailLabel: UILabel!
+    @IBOutlet weak var severeWeatherSwitch: Switch!
+    
+    @IBOutlet weak var billReadyTitleLabel: UILabel!
+    @IBOutlet weak var billReadyDetailLabel: UILabel!
+    @IBOutlet weak var billReadySwitch: Switch!
+    
+    @IBOutlet weak var paymentDueTitleLabel: UILabel!
+    @IBOutlet weak var paymentDueDetailLabel: UILabel!
+    @IBOutlet weak var paymentDueSwitch: Switch!
+    @IBOutlet weak var paymentDueRemindMeLabel: UILabel!
+    @IBOutlet weak var paymentDueDaysBeforeButton: UIButton!
+    
+    @IBOutlet weak var budgetBillingView: UIView!
+    @IBOutlet weak var budgetBillingTitleLabel: UILabel!
+    @IBOutlet weak var budgetBillingSwitch: Switch!
+    
+    @IBOutlet weak var forYourInfoTitleLabel: UILabel!
+    @IBOutlet weak var forYourInfoDetailLabel: UILabel!
+    @IBOutlet weak var forYourInfoSwitch: Switch!
+    
     // Prevents status bar color flash when pushed
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
