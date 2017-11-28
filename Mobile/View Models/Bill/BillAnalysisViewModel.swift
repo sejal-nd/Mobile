ee1113cc6bb4a56c7d7e29003bf90e43a61351b6
Marc Shilling <marcshilling@gmail.com>
2017-11-28 11:03:24 -0500

    a11y update

--- Mobile/View Models/Bill/BillAnalysisViewModel.swift
+++ Mobile/View Models/Bill/BillAnalysisViewModel.swift
@@ -447,16 +447,31 @@ class BillAnalysisViewModel {
             }
 
             var detailString = ""
-            let localizedString = NSLocalizedString("Your bill is projected to be around %@. You've spent about %@ so far this bill period. " +
-                "This is an estimate and the actual amount may vary based on your energy use, taxes, and fees.", comment: "")
-            if let gasForecast = gasForecast, self.isGas {
-                if let projectedCost = gasForecast.projectedCost, let toDateCost = gasForecast.toDateCost {
-                    detailString = String(format: localizedString, projectedCost.currencyString!, toDateCost.currencyString!)
+            if self.accountDetail.isModeledForOpower {
+                let localizedString = NSLocalizedString("Your bill is projected to be around %@. You've spent about %@ so far this bill period. " +
+                    "This is an estimate and the actual amount may vary based on your energy use, taxes, and fees.", comment: "")
+                if let gasForecast = gasForecast, self.isGas {
+                    if let projectedCost = gasForecast.projectedCost, let toDateCost = gasForecast.toDateCost {
+                        detailString = String(format: localizedString, projectedCost.currencyString!, toDateCost.currencyString!)
+                    }
                 }
-            }
-            if let elecForecast = elecForecast, !self.isGas {
-                if let projectedCost = elecForecast.projectedCost, let toDateCost = elecForecast.toDateCost {
-                    detailString = String(format: localizedString, projectedCost.currencyString!, toDateCost.currencyString!)
+                if let elecForecast = elecForecast, !self.isGas {
+                    if let projectedCost = elecForecast.projectedCost, let toDateCost = elecForecast.toDateCost {
+                        detailString = String(format: localizedString, projectedCost.currencyString!, toDateCost.currencyString!)
+                    }
+                }
+            } else {
+                let localizedString = NSLocalizedString("You are projected to use around %d %@. You've used about %d %@ so far this bill period. This is an estimate and the actual amount may vary.", comment: "")
+                let meterUnit = billComparison.meterUnit
+                if let gasForecast = gasForecast, self.isGas {
+                    if let projectedUsage = gasForecast.projectedUsage, let toDateUsage = gasForecast.toDateUsage {
+                        detailString = String(format: localizedString, Int(projectedUsage), meterUnit, Int(toDateUsage), meterUnit)
+                    }
+                }
+                if let elecForecast = elecForecast, !self.isGas {
+                    if let projectedUsage = elecForecast.projectedUsage, let toDateUsage = elecForecast.toDateUsage {
+                        detailString = String(format: localizedString, Int(projectedUsage), meterUnit, Int(toDateUsage), meterUnit)
+                    }
                 }
             }
 
