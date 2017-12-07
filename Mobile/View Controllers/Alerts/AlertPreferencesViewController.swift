748e6a69c1ebdfba4c1ad7fd017c416f95aa2ea0
Marc Shilling <marcshilling@gmail.com>
2017-12-07 15:40:56 -0500

    fix failed a11y issues

--- Mobile/View Controllers/Alerts/AlertPreferencesViewController.swift
+++ Mobile/View Controllers/Alerts/AlertPreferencesViewController.swift
@@ -147,6 +147,7 @@ class AlertPreferencesViewController: UIViewController {
         notificationsDisabledButton.setTitleColor(.actionBlue, for: .normal)
         notificationsDisabledButton.titleLabel?.font = SystemFont.medium.of(textStyle: .headline)
         notificationsDisabledButton.titleLabel?.text = NSLocalizedString("Go to Settings", comment: "")
+        notificationsDisabledView.isHidden = true // Hide it initially so VoiceOver doesn't immediately pick up. bindViewModel() will handle actual hide/show logic
         
         outageTitleLabel.textColor = .blackText
         outageTitleLabel.font = SystemFont.regular.of(textStyle: .title1)
@@ -221,8 +222,6 @@ class AlertPreferencesViewController: UIViewController {
         viewModel.isFetching.asDriver().not().drive(loadingIndicator.rx.isHidden).disposed(by: disposeBag)
         viewModel.isError.asDriver().not().drive(errorLabel.rx.isHidden).disposed(by: disposeBag)
         viewModel.devicePushNotificationsEnabled.asDriver().drive(notificationsDisabledView.rx.isHidden).disposed(by: disposeBag)
-        viewModel.devicePushNotificationsEnabled.asDriver().not().drive(notificationsDisabledLabel.rx.isAccessibilityElement).disposed(by: disposeBag)
-        viewModel.devicePushNotificationsEnabled.asDriver().not().drive(notificationsDisabledButton.rx.isAccessibilityElement).disposed(by: disposeBag)
         viewModel.shouldShowContent.not().drive(contentStackView.rx.isHidden).disposed(by: disposeBag)
         viewModel.saveButtonEnabled.drive(saveButton.rx.isEnabled).disposed(by: disposeBag)
         
@@ -358,17 +357,6 @@ class AlertPreferencesViewController: UIViewController {
     }
     
     @objc func onSavePress() {
-//        print("outage = \(viewModel.outage.value)")
-//        print("scheduledMaint = \(viewModel.scheduledMaint.value)")
-//        print("severeWeather = \(viewModel.severeWeather.value)")
-//        print("billReady = \(viewModel.billReady.value)")
-//        print("paymentDue = \(viewModel.paymentDue.value)")
-//        print("paymentDueDaysBefore = \(viewModel.paymentDueDaysBefore.value)")
-//        print("budgetBilling = \(viewModel.budgetBilling.value)")
-//        print("forYourInfo = \(viewModel.forYourInfo.value)")
-//        print("shouldEnrollPaperlessEBill = \(viewModel.shouldEnrollPaperlessEBill)")
-//        print("shouldUnenrollPaperlessEBill = \(viewModel.shouldUnenrollPaperlessEBill)")
-        
         Analytics().logScreenView(AnalyticsPageView.AlertsPrefCenterSave.rawValue)
         
         LoadingView.show()
