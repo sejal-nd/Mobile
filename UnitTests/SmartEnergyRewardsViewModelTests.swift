be285db3cd6ad6a1cc98754b3b477a2f7c1ddd5c
Marc Shilling <marcshilling@gmail.com>
2018-02-27 18:06:48 -0500

    more SER tests

--- UnitTests/SmartEnergyRewardsViewModelTests.swift
+++ UnitTests/SmartEnergyRewardsViewModelTests.swift
@@ -9,6 +9,7 @@
 import XCTest
 import RxSwift
 import RxTest
+import RxCocoa
 
 class SmartEnergyRewardsViewModelTests: XCTestCase {
     
@@ -24,10 +25,20 @@ class SmartEnergyRewardsViewModelTests: XCTestCase {
         return dateFormatter
     }()
     
+    lazy var hourDateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = .opCo
+        dateFormatter.dateFormat = "MM/dd/yyyy HH:mm"
+        return dateFormatter
+    }()
+    
     override func setUp() {
         super.setUp()
-        viewModel = SmartEnergyRewardsViewModel(accountDetailDriver: accountDetailSubject.asDriver(onErrorDriveWith: .empty()))
         
+        viewModel = SmartEnergyRewardsViewModel(accountDetailDriver: accountDetailSubject.asDriver(onErrorDriveWith: .empty()))
+    }
+    
+    private func scheduleBoilerplateEvents() {
         let eventResults2 = [
             SERResult(eventStart: dateFormatter.date(from: "03/21/2016")!),
             SERResult(eventStart: dateFormatter.date(from: "05/23/2017")!),
@@ -35,13 +46,9 @@ class SmartEnergyRewardsViewModelTests: XCTestCase {
             SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!)
         ]
         
-        let eventStart = dateFormatter.date(from: "04/23/2018")!
-        let endTimeAddComponents = DateComponents(calendar: Calendar.opCo, timeZone: .opCo, hour: 17)
-        let eventEnd = Calendar.opCo.date(byAdding: endTimeAddComponents, to: eventStart)!
-        
         let eventResults4 = [
             SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!),
-            SERResult(eventStart: eventStart, eventEnd: eventEnd),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!),
             SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!),
             SERResult(eventStart: dateFormatter.date(from: "06/23/2018")!)
         ]
@@ -61,6 +68,8 @@ class SmartEnergyRewardsViewModelTests: XCTestCase {
     }
     
     func testLatest3EventsThisSeason() {
+        scheduleBoilerplateEvents()
+        
         let expectedEvents = [
             [],
             [
@@ -89,6 +98,8 @@ class SmartEnergyRewardsViewModelTests: XCTestCase {
     }
     
     func testNumBarsToShow() {
+        scheduleBoilerplateEvents()
+        
         let observer = scheduler.createObserver(Int.self)
         viewModel.numBarsToShow.drive(observer).disposed(by: disposeBag)
         
@@ -98,6 +109,8 @@ class SmartEnergyRewardsViewModelTests: XCTestCase {
     }
     
     func testShouldShowBar1() {
+        scheduleBoilerplateEvents()
+        
         let observer = scheduler.createObserver(Bool.self)
         viewModel.shouldShowBar1.drive(observer).disposed(by: disposeBag)
         
@@ -107,6 +120,8 @@ class SmartEnergyRewardsViewModelTests: XCTestCase {
     }
     
     func testShouldShowBar2() {
+        scheduleBoilerplateEvents()
+        
         let observer = scheduler.createObserver(Bool.self)
         viewModel.shouldShowBar2.drive(observer).disposed(by: disposeBag)
         
@@ -116,12 +131,61 @@ class SmartEnergyRewardsViewModelTests: XCTestCase {
     }
     
     func testShouldShowBar3() {
+        scheduleBoilerplateEvents()
+        
         let observer = scheduler.createObserver(Bool.self)
         viewModel.shouldShowBar3.drive(observer).disposed(by: disposeBag)
         
         scheduler.start()
         
         XCTAssertEqual(observer.events, [false, true, true, true].enumerated().map(next))
+    }
+    
+    func testBar1DollarLabelText() {
+        let eventResults1 = [
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 2),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 1)
+        ]
+        
+        let eventResults2 = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 2),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 4),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 1)
+        ]
+        
+        let eventResults3 = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 13.82),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 121),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 2)
+        ]
+        
+        let events = [
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults1)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults2)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults3))
+        ].enumerated().map(next)
+        
+        scheduler.createHotObservable(events)
+            .bind(to: accountDetailSubject)
+            .disposed(by: disposeBag)
+        
+        let observer = scheduler.createObserver(String?.self)
+        viewModel.bar1DollarLabelText.drive(observer).disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(observer.events, [nil, "$2.00", "$13.82"].enumerated().map(next))
+    }
+    
+    func testBar1DateLabelText() {
+        scheduleBoilerplateEvents()
+        
+        let observer = scheduler.createObserver(String?.self)
+        viewModel.bar1DateLabelText.drive(observer).disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(observer.events, [nil, nil, nil, "APR 23"].enumerated().map(next))
     }
     
     func testBar1HeightConstraintValue() {
@@ -164,7 +228,6 @@ class SmartEnergyRewardsViewModelTests: XCTestCase {
             AccountDetail(serInfo: SERInfo(eventResults: eventResults5))
             ]
             .enumerated()
-            .map { ($0.0 + 4, $0.1) }
             .map(next)
         
         scheduler.createHotObservable(events)
@@ -176,21 +239,289 @@ class SmartEnergyRewardsViewModelTests: XCTestCase {
         
         scheduler.start()
         
-        XCTAssertEqual(observer.events, [0, 0, 0, 121, 121, 60.5, 3, 60.5, 3].enumerated().map(next))
+        XCTAssertEqual(observer.events, [121, 60.5, 3, 60.5, 3].enumerated().map(next))
     }
     
-    func testBar1A11yLabel() {
+    func testBar2DollarLabelText() {
+        let eventResults1 = [
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 2)
+        ]
+        
+        let eventResults2 = [
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 2),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 1)
+        ]
+        
+        let eventResults3 = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 2),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 14.21),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 4)
+        ]
+        
+
+        let events = [
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults1)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults2)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults3))
+        ].enumerated().map(next)
+        
+        scheduler.createHotObservable(events)
+            .bind(to: accountDetailSubject)
+            .disposed(by: disposeBag)
+        
         let observer = scheduler.createObserver(String?.self)
-        viewModel.bar1A11yLabel.drive(observer).disposed(by: disposeBag)
+        viewModel.bar2DollarLabelText.drive(observer).disposed(by: disposeBag)
         
         scheduler.start()
         
-        let expectedStrings = [
-            nil, nil, nil,
-            "April 23, 2018. Peak Hours: 12AM - 5PM. Typical use: 0.0 kWh. Actual use: 0.0 kWh. Energy savings: 0.0 kWh. Bill credit: $0.00"
+        XCTAssertEqual(observer.events, [nil, "$2.00", "$14.21"].enumerated().map(next))
+    }
+    
+    func testBar2DateLabelText() {
+        scheduleBoilerplateEvents()
+        
+        let observer = scheduler.createObserver(String?.self)
+        viewModel.bar2DateLabelText.drive(observer).disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(observer.events, [nil, nil, "FEB 23", "MAY 23"].enumerated().map(next))
+    }
+    
+    func testBar2HeightConstraintValue() {
+        
+        let eventResults1 = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 2),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 3),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 1)
         ]
         
-        XCTAssertEqual(observer.events, expectedStrings.enumerated().map(next))
+        let eventResults2 = [
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 4),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 1),
+            SERResult(eventStart: dateFormatter.date(from: "06/23/2018")!, savingDollar: 1)
+        ]
+        
+        let eventResults3 = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 1),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 3),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 5)
+        ]
+        
+        let eventResults4 = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 3),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 2),
+        ]
+        
+        let eventResults5 = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 3),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 6),
+        ]
+        
+        let events = [
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults1)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults2)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults3)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults4)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults5))
+            ]
+            .enumerated()
+            .map(next)
+        
+        scheduler.createHotObservable(events)
+            .bind(to: accountDetailSubject)
+            .disposed(by: disposeBag)
+        
+        let observer = scheduler.createObserver(CGFloat.self)
+        viewModel.bar2HeightConstraintValue.drive(observer).disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(observer.events, [121, 30.25, 72.6, 121, 60.5].enumerated().map(next))
+    }
+    
+    func testBar3DollarLabelText() {
+        let eventResults1 = [
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 2)
+        ]
+        
+        let eventResults2 = [
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 2),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 1)
+        ]
+        
+        let eventResults3 = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 2),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 3),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 182.99)
+        ]
+        
+        
+        let events = [
+            AccountDetail(serInfo: SERInfo(eventResults: [])),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults1)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults2)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults3))
+            ].enumerated().map(next)
+        
+        scheduler.createHotObservable(events)
+            .bind(to: accountDetailSubject)
+            .disposed(by: disposeBag)
+        
+        let observer = scheduler.createObserver(String?.self)
+        viewModel.bar3DollarLabelText.drive(observer).disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(observer.events, [nil, "$2.00", "$1.00", "$182.99"].enumerated().map(next))
+    }
+    
+    func testBar3DateLabelText() {
+        scheduleBoilerplateEvents()
+        
+        let observer = scheduler.createObserver(String?.self)
+        viewModel.bar3DateLabelText.drive(observer).disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(observer.events, [nil, "MAY 23", "MAY 23", "JUN 23"].enumerated().map(next))
+    }
+    
+    func testBar3HeightConstraintValue() {
+        
+        let eventResults1 = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 1),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 2),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 3)
+        ]
+        
+        let eventResults2 = [
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 4),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 1),
+            SERResult(eventStart: dateFormatter.date(from: "06/23/2018")!, savingDollar: 1)
+        ]
+        
+        let eventResults3 = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 1),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 4),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 2)
+        ]
+        
+        let eventResults4 = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 2),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 3)
+        ]
+        
+        let eventResults5 = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 300),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 1)
+        ]
+        
+        let eventResults6 = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 3),
+        ]
+        
+        let events = [
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults1)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults2)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults3)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults4)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults5)),
+            AccountDetail(serInfo: SERInfo(eventResults: eventResults6))
+            ]
+            .enumerated()
+            .map(next)
+        
+        scheduler.createHotObservable(events)
+            .bind(to: accountDetailSubject)
+            .disposed(by: disposeBag)
+        
+        let observer = scheduler.createObserver(CGFloat.self)
+        viewModel.bar3HeightConstraintValue.drive(observer).disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(observer.events, [121, 30.25, 60.5, 121, 3, 121].enumerated().map(next))
+    }
+    
+    func testBarDescriptionDateLabelText() {
+        let eventResults = [
+            SERResult(eventStart: dateFormatter.date(from: "03/21/2018")!, savingDollar: 2),
+            SERResult(eventStart: dateFormatter.date(from: "04/23/2018")!, savingDollar: 3),
+            SERResult(eventStart: dateFormatter.date(from: "05/23/2018")!, savingDollar: 4)
+        ]
+        
+        let accountDetailDriver = Driver.just(AccountDetail(serInfo: SERInfo(eventResults: eventResults)))
+        viewModel = SmartEnergyRewardsViewModel(accountDetailDriver: accountDetailDriver)
+        
+        viewModel.barDescriptionDateLabelText.asObservable().take(1).subscribe(onNext: { text in
+            XCTAssertEqual(text, "May 23, 2018")
+        }).disposed(by: disposeBag)
+        
+        viewModel.setBarSelected(tag: 0)
+        viewModel.barDescriptionDateLabelText.asObservable().take(1).subscribe(onNext: { text in
+            XCTAssertEqual(text, "March 21, 2018")
+        }).disposed(by: disposeBag)
+        
+        viewModel.setBarSelected(tag: 1)
+        viewModel.barDescriptionDateLabelText.asObservable().take(1).subscribe(onNext: { text in
+            XCTAssertEqual(text, "April 23, 2018")
+        }).disposed(by: disposeBag)
+    }
+    
+    func testBarDescriptionPeakHoursLabelText() {
+        let startDate = hourDateFormatter.date(from: "03/21/2018 12:00")!
+        let endDate = hourDateFormatter.date(from: "03/21/2018 19:00")!
+        let eventResults = [
+            SERResult(eventStart: startDate, eventEnd: endDate)
+        ]
+        
+        let accountDetailDriver = Driver.just(AccountDetail(serInfo: SERInfo(eventResults: eventResults)))
+        viewModel = SmartEnergyRewardsViewModel(accountDetailDriver: accountDetailDriver)
+        
+        viewModel.barDescriptionPeakHoursLabelText.asObservable().take(1).subscribe(onNext: { text in
+            XCTAssertEqual(text, "Peak Hours: 12PM - 7PM")
+        }).disposed(by: disposeBag)
+    }
+    
+    func testBarDescriptionTypicalUseValueLabelText() {
+        
+    }
+    
+    func testBarDescriptionActualUseValueLabelText() {
+        
+    }
+    
+    func testBarDescriptionEnergySavingsValueLabelText() {
+        
+    }
+    
+    func testBarDescriptionBillCreditValueLabelText() {
+        
+    }
+    
+    func testSetBarSelected() {
+        for i in stride(from: 0, to: 3, by: 1) {
+            if viewModel.barGraphSelectionStates.value[i].value && i != 2 {
+                XCTFail("All variables should be false initially except index 2")
+            }
+        }
+        
+        viewModel.setBarSelected(tag: 0)
+        XCTAssert(viewModel.barGraphSelectionStates.value[0].value, "Index 0's value should be true")
+        for i in stride(from: 0, to: 3, by: 1) {
+            if viewModel.barGraphSelectionStates.value[i].value && i != 0 {
+                XCTFail("All variables should be false except index 0")
+            }
+        }
+        
+        viewModel.setBarSelected(tag: 1)
+        XCTAssert(viewModel.barGraphSelectionStates.value[1].value, "Index 1's value should be true")
+        for i in stride(from: 0, to: 3, by: 1) {
+            if viewModel.barGraphSelectionStates.value[i].value && i != 1 {
+                XCTFail("All variables should be false except index 1")
+            }
+        }
     }
     
 }
