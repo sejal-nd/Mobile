beb5ed66dd386610256334f5e97fc191ec1aea1f
Sam Francis <sfrancis@mindgrub.com>
2018-02-27 14:30:46 -0500

    Created peak rewards test file

---
+++ UnitTests/Peak Rewards/PeakRewardsViewModelTests.swift
@@ -0,0 +1,28 @@
+//
+//  PeakRewardsViewModelTests.swift
+//  Mobile
+//
+//  Created by Samuel Francis on 2/27/18.
+//  Copyright © 2018 Exelon Corporation. All rights reserved.
+//
+
+import XCTest
+import RxSwift
+import RxTest
+
+class PeakRewardsViewModelTests: XCTestCase {
+    
+    let disposeBag = DisposeBag()
+    var scheduler = TestScheduler(initialClock: 0)
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
