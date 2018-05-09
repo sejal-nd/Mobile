76efb7a1a47ee9f90e4f2a1d83bea65b75e30514
Sam Francis <sfrancis@mindgrub.com>
2018-05-09 17:58:35 -0400

    Added MM check to unauth outage workflow

--- Mobile/Views/MaintenanceModeView/MaintenanceModeView.swift
+++ Mobile/Views/MaintenanceModeView/MaintenanceModeView.swift
@@ -12,6 +12,7 @@ import RxCocoa
 
 class MaintenanceModeView: UIView {
     
+    @IBOutlet weak var gradientView: UIView!
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var maintenanceImageView: UIImageView!
     @IBOutlet weak var reloadButton: ButtonControl!
@@ -22,19 +23,20 @@ class MaintenanceModeView: UIView {
     @IBOutlet weak var infoTextContainerView: UIView!
     @IBOutlet weak var infoTextView: DataDetectorTextView!
     
+    var gradientLayer: CAGradientLayer?
+    
     let disposeBag = DisposeBag()
 
     @IBInspectable var isColorBackground: Bool = true {
         didSet {
+            gradientView.isHidden = isColorBackground
             if isColorBackground {
-                containerView.backgroundColor = .primaryColor
                 maintenanceImageView.image = #imageLiteral(resourceName: "ic_maint_mode_white")
                 reloadImageView.image = #imageLiteral(resourceName: "ic_reload")
                 reloadLabel.textColor = .white
                 scheduledMaintenanceLabel.textColor = .white
                 detailLabel.textColor = .white
             } else {
-                containerView.backgroundColor = .clear
                 maintenanceImageView.image = #imageLiteral(resourceName: "ic_maint_mode")
                 reloadImageView.image = #imageLiteral(resourceName: "ic_reload_blue")
                 reloadLabel.textColor = .actionBlue
@@ -80,6 +82,22 @@ class MaintenanceModeView: UIView {
         super.layoutSubviews()
         styleViews()
         infoTextView.attributedText = infoText
+        
+        if isColorBackground {
+            gradientLayer?.removeFromSuperlayer()
+            gradientLayer = nil
+        } else {
+            let gradientLayer = CAGradientLayer()
+            gradientLayer.frame = bounds
+            gradientLayer.colors = [
+                UIColor.white.cgColor,
+                UIColor(red: 244/255, green: 246/255, blue: 247/255, alpha: 1).cgColor,
+                UIColor(red: 240/255, green: 242/255, blue: 243/255, alpha: 1).cgColor
+            ]
+            gradientLayer.locations = [0.0, 0.5, 1.0]
+            gradientView.layer.addSublayer(gradientLayer)
+            self.gradientLayer = gradientLayer
+        }
     }
     
     func styleViews() {
