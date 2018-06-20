ab4580454a340b38b7fe8744fbdb26492d3718c7
Sam Francis <sfrancis@mindgrub.com>
2018-06-20 00:52:24 -0400

    Changed removeAll(where:) from "deprecated" to "obsoleted" in swift 4.2

--- Mobile/Extensions/ArrayRemoveAllWhere.swift
+++ Mobile/Extensions/ArrayRemoveAllWhere.swift
@@ -8,13 +8,14 @@
 
 import Foundation
 
-@available(swift, deprecated: 4.2, message: "`removeAll(where:)` is in the Swift 4.2 standard library, so we can remove this when we update.")
 extension Array {
+    @available(swift, obsoleted: 4.2, message: "`removeAll(where:)` is in the Swift 4.2 standard library, so we can remove this when we update.")
     mutating func removeAll(where shouldRemove: (Element) -> Bool) {
         let partitionIndex = halfStablePartition(isSuffixElement: shouldRemove)
         removeSubrange(partitionIndex...)
     }
     
+    @available(swift, obsoleted: 4.2, message: "`removeAll(where:)` is in the Swift 4.2 standard library, so we can remove this when we update.")
     mutating func halfStablePartition(isSuffixElement: (Element) -> Bool) -> Index {
         guard var i = enumerated().first(where: { isSuffixElement($1) })?.0 else { return endIndex }
         var j = index(after: i)
@@ -24,12 +25,11 @@ extension Array {
         }
         return i
     }
-}
-
-extension Array {
+    
     func removingAll(where shouldRemove: (Element) -> Bool) -> [Element] {
         var a = self
         a.removeAll(where: shouldRemove)
         return a
     }
 }
+
