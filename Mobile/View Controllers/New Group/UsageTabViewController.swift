1d19d4efbfd7524cff9d5d87db9d1977de171c34
Joseph Erlandson <joseph.erlandson@exeloncorp.com>
2018-07-12 13:53:54 -0400

    removed Alerts tab, added usage tab & root view

---
+++ Mobile/View Controllers/New Group/UsageTabViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UsageTabViewController.swift
+//  BGE
+//
+//  Created by Joseph Erlandson on 7/12/18.
+//  Copyright © 2018 Exelon Corporation. All rights reserved.
+//
+
+import UIKit
+
+class UsageTabViewController: UIViewController {
+    
+    
+    
+}
