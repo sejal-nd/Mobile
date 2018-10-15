d1e5052f1822713bc86fe9c6ce0d4f9ca5377a36
Sam Francis <sfrancis@mindgrub.com>
2018-10-15 15:18:40 -0400

    Made MockAppointmentService more robust for testing

--- Mobile/Core/Service/Mock/MockAppointmentService.swift
+++ Mobile/Core/Service/Mock/MockAppointmentService.swift
@@ -10,49 +10,50 @@ import RxSwift
 
 class MockAppointmentService: AppointmentService {
     func fetchAppointments(accountNumber: String, premiseNumber: String) -> Observable<[Appointment]> {
-        let accountIndex = AccountsStore.shared.accounts.index(of: AccountsStore.shared.currentAccount)
+        let loggedInUsername = UserDefaults.standard.string(forKey: UserDefaultKeys.loggedInUsername)
         
-        let startTime = Calendar.opCo.startOfDay(for: Date()).addingTimeInterval(43_200)
-        let endTime = startTime.addingTimeInterval(3600)
-        switch accountIndex {
-        case 0:
-            return Observable<[Appointment]>
-                .just([Appointment(startTime: startTime,
-                                   endTime: endTime,
-                                   status: .scheduled,
-                                   caseNumber: "0"),
-                       Appointment(startTime: startTime,
-                                   endTime: endTime,
-                                   status: .enRoute,
-                                   caseNumber: "0"),
-                       Appointment(startTime: startTime,
-                                   endTime: endTime,
-                                   status: .inProgress,
-                                   caseNumber: "0"),
-                       Appointment(startTime: startTime,
-                                   endTime: endTime,
-                                   status: .complete,
-                                   caseNumber: "0"),
-                       Appointment(startTime: startTime,
-                                   endTime: endTime,
-                                   status: .canceled,
-                                   caseNumber: "0")])
-        case 2:
-            return .just([Appointment(startTime: startTime,
-                                      endTime: endTime,
-                                      status: .canceled,
-                                      caseNumber: "1")])
-        case 1:
-            return .just([Appointment(startTime: startTime,
-                                      endTime: endTime,
-                                      status: .inProgress,
-                                      caseNumber: "2"),
-                          Appointment(startTime: startTime.addingTimeInterval(86_400),
-                                      endTime: endTime.addingTimeInterval(86_400),
-                                      status: .canceled,
-                                      caseNumber: "3")])
+        let noon = Calendar.opCo.date(byAdding: DateComponents(hour: 12),
+                                      to: Calendar.opCo.startOfDay(for: Date()))!
+        let one = Calendar.opCo.date(byAdding: DateComponents(hour: 1), to: noon)!
+        
+        let appointments: [Appointment]
+        switch loggedInUsername {
+        case "apptToday":
+            appointments = [Appointment(startTime: noon, endTime: one, status: .scheduled, caseNumber: "0")]
+        case "apptTomorrow":
+            let noonTomorrow = Calendar.opCo.date(byAdding: DateComponents(day: 1), to: noon)!
+            let oneTomorrow = Calendar.opCo.date(byAdding: DateComponents(day: 1), to: one)!
+            
+            appointments = [Appointment(startTime: noonTomorrow, endTime: oneTomorrow, status: .scheduled, caseNumber: "0")]
+        case "apptScheduled":
+            let noonLaterDate = Calendar.opCo.date(byAdding: DateComponents(day: 5), to: noon)!
+            let oneLaterDate = Calendar.opCo.date(byAdding: DateComponents(day: 5), to: one)!
+            
+            appointments = [Appointment(startTime: noonLaterDate, endTime: oneLaterDate, status: .scheduled, caseNumber: "0")]
+        case "apptEnRoute":
+            appointments = [Appointment(startTime: noon, endTime: one, status: .enRoute, caseNumber: "0")]
+        case "apptInProgress":
+            appointments = [Appointment(startTime: noon, endTime: one, status: .inProgress, caseNumber: "0")]
+        case "apptComplete":
+            appointments = [Appointment(startTime: noon, endTime: one, status: .complete, caseNumber: "0")]
+        case "apptCanceled":
+            appointments = [Appointment(startTime: noon, endTime: one, status: .canceled, caseNumber: "0")]
+        case "apptMultiple":
+            appointments = [
+                Appointment(startTime: noon, endTime: one, status: .scheduled, caseNumber: "01234"),
+                Appointment(startTime: noon, endTime: one, status: .enRoute, caseNumber: "43210"),
+                Appointment(startTime: noon, endTime: one, status: .inProgress, caseNumber: "56789"),
+                Appointment(startTime: noon, endTime: one, status: .complete, caseNumber: "98765"),
+                Appointment(startTime: noon, endTime: one, status: .canceled, caseNumber: "02468")
+            ]
+        case "apptFailure":
+            return .error(ServiceError(serviceCode: ServiceErrorCode.localError.rawValue))
+        case "apptNone":
+            fallthrough
         default:
-            return .just([])
+            appointments = []
         }
+        
+        return .just(appointments)
     }
 }
