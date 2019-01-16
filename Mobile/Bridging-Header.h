f16665a3fd7f0e8668343d653d5ff31bda4208e4
Joseph Erlandson <joseph.erlandson@exeloncorp.com>
2019-01-16 09:32:45 -0500

    fixed proj file
    updated objc bridging header to remove unused imports

--- Mobile/Bridging-Header.h
+++ Mobile/Bridging-Header.h
@@ -4,17 +4,10 @@
 
 #ifndef EXBridgingHeader_h
 #define EXBridgingHeader_h
-
-//#import "A0SimpleKeychain.h"
-
 #import "CardIO.h"
-
-//#import <PDTSimpleCalendar/PDTSimpleCalendar.h>
 
 #import "GAI.h"
 #import "GAIDictionaryBuilder.h"
 #import "GAIFields.h"
 #import "GAILogger.h"
-
-//#import "Firebase.h"
 #endif /* EXBridgingHeader_h */
