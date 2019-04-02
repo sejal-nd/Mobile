c095d9b0e41c6bd39bc5155cdaf7b8b353755954
Marc Shilling <mshilling@mindgrub.com>
2019-04-02 14:45:24 -0400

    fix crash due to changed storyboards - must have got lost in the merge. Also, indent fixing

--- Mobile/View Controllers/Bill/Payment/ReviewPaymentViewController.swift
+++ Mobile/View Controllers/Bill/Payment/ReviewPaymentViewController.swift
@@ -267,24 +267,24 @@ class ReviewPaymentViewController: UIViewController {
             guard let self = self else { return }
             
             LoadingView.hide()
-                let paymentusAlertVC = UIAlertController.paymentusErrorAlertController(
-                    forError: err,
-                    walletItem: self.viewModel.selectedWalletItem.value!,
-                    okHandler: { [weak self] _ in
-                        guard let self = self else { return }
-                        if err.serviceCode == ServiceErrorCode.walletItemIdTimeout.rawValue {
-                            guard let navCtl = self.navigationController else { return }
-                            let makePaymentVC = UIStoryboard(name: "Wallet", bundle: nil)
-                                .instantiateViewController(withIdentifier: "makeAPayment") as! MakePaymentViewController
-                            makePaymentVC.accountDetail = self.viewModel.accountDetail.value
-                            navCtl.viewControllers = [navCtl.viewControllers.first!, makePaymentVC]
-                        }
-                    },
-                    callHandler: { _ in
-                        UIApplication.shared.openPhoneNumberIfCan(self.viewModel.errorPhoneNumber)
+            let paymentusAlertVC = UIAlertController.paymentusErrorAlertController(
+                forError: err,
+                walletItem: self.viewModel.selectedWalletItem.value!,
+                okHandler: { [weak self] _ in
+                    guard let self = self else { return }
+                    if err.serviceCode == ServiceErrorCode.walletItemIdTimeout.rawValue {
+                        guard let navCtl = self.navigationController else { return }
+                        let makePaymentVC = UIStoryboard(name: "Payment", bundle: nil)
+                            .instantiateInitialViewController() as! MakePaymentViewController
+                        makePaymentVC.accountDetail = self.viewModel.accountDetail.value
+                        navCtl.viewControllers = [navCtl.viewControllers.first!, makePaymentVC]
+                    }
+                },
+                callHandler: { _ in
+                    UIApplication.shared.openPhoneNumberIfCan(self.viewModel.errorPhoneNumber)
                 }
-                )
-                self.present(paymentusAlertVC, animated: true, completion: nil)
+            )
+            self.present(paymentusAlertVC, animated: true, completion: nil)
         }
         
         if viewModel.paymentId.value != nil { // Modify
