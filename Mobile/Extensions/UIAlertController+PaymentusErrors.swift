9115ad5d0d8c07279d5b1c5d50f62fa8888154cd
Marc Shilling <mshilling@mindgrub.com>
2019-04-10 15:20:18 -0400

    update the parameter name for clarity

--- Mobile/Extensions/UIAlertController+PaymentusErrors.swift
+++ Mobile/Extensions/UIAlertController+PaymentusErrors.swift
@@ -11,7 +11,7 @@ import UIKit
 extension UIAlertController {
     static func paymentusErrorAlertController(forError error: ServiceError,
                                               walletItem: WalletItem,
-                                              customMessage: String? = nil,
+                                              customMessageForSessionExpired: String? = nil,
                                               okHandler: @escaping ((UIAlertAction) -> ()) = { _ in },
                                               callHandler: @escaping ((UIAlertAction) -> ())) -> UIAlertController {
         let title: String
@@ -52,12 +52,11 @@ extension UIAlertController {
             includeCallCTA = true
         case ServiceErrorCode.walletItemIdTimeout.rawValue:
             title = NSLocalizedString("Session Expired", comment: "")
-            message = customMessage ?? NSLocalizedString("Please attempt to make your payment again.", comment: "")
+            message = customMessageForSessionExpired ?? NSLocalizedString("Please attempt to make your payment again.", comment: "")
         default:
             title = NSLocalizedString("Payment Error", comment: "")
             message = NSLocalizedString("Unable to process electronic payments for your account at this time. Please try again later or view other payment options.", comment: "")
         }
-        
         
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         if includeCallCTA {
