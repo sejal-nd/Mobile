b5f3171984dcba62c14243b4b2070189b5aae2ba
Joseph Erlandson <joseph.erlandson@exeloncorp.com>
2019-07-08 12:28:29 -0400

    added if wrap to fix unit tests should update tests to execute on iOS 11+ only.

--- Mobile/Extensions/UINavigationController+Style.swift
+++ Mobile/Extensions/UINavigationController+Style.swift
@@ -18,12 +18,14 @@ extension UINavigationController {
         navigationBar.setBackgroundImage(nil, for: .default)
         navigationBar.shadowImage = UIColor.primaryColor.image()
         
-        navigationBar.largeTitleTextAttributes = [
-            .foregroundColor: StormModeStatus.shared.isOn ? UIColor.white : UIColor.blackText,
-            .font: OpenSans.bold.of(size: 20)
-        ]
-        navigationItem.largeTitleDisplayMode = .always
-        navigationBar.prefersLargeTitles = true
+        if #available(iOS 11.0, *) {
+            navigationBar.largeTitleTextAttributes = [
+                .foregroundColor: StormModeStatus.shared.isOn ? UIColor.white : UIColor.blackText,
+                .font: OpenSans.bold.of(size: 20)
+            ]
+            navigationItem.largeTitleDisplayMode = .always
+            navigationBar.prefersLargeTitles = true
+        }
 
         navigationBar.titleTextAttributes = [
             .foregroundColor: StormModeStatus.shared.isOn ? UIColor.white : UIColor.blackText,
