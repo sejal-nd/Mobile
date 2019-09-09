9a2b41439b650ac91dee49f1f46f7f7c2a83b0c3
Joseph Erlandson <joseph.erlandson@exeloncorp.com>
2019-09-09 09:40:48 -0400

    changed alret controller to info alert in proj bbill ? press

--- Mobile/Feature/Home/HomeViewController.swift
+++ Mobile/Feature/Home/HomeViewController.swift
@@ -570,11 +570,9 @@ class HomeViewController: AccountPickerViewController {
             }).disposed(by: projectedBillCardView.disposeBag)
         
         projectedBillCardView.infoButton.rx.touchUpInside.asDriver().drive(onNext: { [weak self] in
-            let alertVc = UIAlertController(title: NSLocalizedString("Estimated Amount", comment: ""),
-                                            message: NSLocalizedString("This is an estimate and the actual amount may vary based on your energy use, taxes, and fees.", comment: ""),
-                                            preferredStyle: .alert)
-            alertVc.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil))
-            self?.present(alertVc, animated: true, completion: nil)
+            let alertViewController = InfoAlertController(title: NSLocalizedString("Estimated Amount", comment: ""),
+            message: NSLocalizedString("This is an estimate and the actual amount may vary based on your energy use, taxes, and fees.", comment: ""))
+            self?.present(alertViewController, animated: true, completion: nil)
         }).disposed(by: projectedBillCardView.disposeBag)
     }
     
