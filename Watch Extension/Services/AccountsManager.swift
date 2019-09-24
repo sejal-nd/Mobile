6f49f717dd2e5301ff499f6664c8cdf517af38a6
Joseph Erlandson <joseph.erlandson@exeloncorp.com>
2019-09-24 07:27:03 -0400

    moved fetch accounts

--- Watch Extension/Services/AccountsManager.swift
+++ Watch Extension/Services/AccountsManager.swift
@@ -13,39 +13,6 @@ struct AccountsManager {
     
     private let disposeBag = DisposeBag()
 
-    public func fetchAccounts(success: @escaping ([Account]) -> Void, error: @escaping (ServiceError) -> Void) {
-        dLog("Fetching Accounts...")
-        
-        guard KeychainUtility.shared[keychainKeys.authToken] != nil else {
-            dLog("Could not find auth token in Accounts Manager Fetch Accounts.")
-            error(Errors.noAuthTokenFound)
-            return
-        }
-        
-        let accountService = MCSAccountService()
-        accountService.fetchAccounts().subscribe(onNext: { accounts in
-            // handle success
-            guard let firstAccount = AccountsStore.shared.accounts.first else {
-                dLog("Could not retrieve first account in account list.")
-                return
-            }
-            
-            if AccountsStore.shared.currentIndex == nil {
-                AccountsStore.shared.currentIndex = 0
-            }
-            
-            NotificationCenter.default.post(name: Notification.Name.defaultAccountSet, object: firstAccount)
-            
-            dLog("Accounts Fetched.")
-            
-            success(accounts)
-        }, onError: { accountsError in
-            // handle error
-            dLog("Failed to retrieve accounts: \(accountsError.localizedDescription)")
-            let serviceError = (accountsError as? ServiceError) ?? ServiceError(serviceCode: accountsError.localizedDescription, serviceMessage: nil, cause: nil)
-            error(serviceError)
-        })
-            .disposed(by: disposeBag)
-    }
+
     
 }
