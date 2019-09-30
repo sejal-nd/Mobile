fecb8e3c6ced5431e22e361a20a80b0de9d6f315
Joseph Erlandson <joseph.erlandson@exeloncorp.com>
2019-09-30 14:40:26 -0400

    fixed outage map alert

--- Mobile/Feature/Outage/Detail Pages/OutageMapViewController.swift
+++ Mobile/Feature/Outage/Detail Pages/OutageMapViewController.swift
@@ -68,7 +68,7 @@ class OutageMapViewController: UIViewController {
             GoogleAnalytics.log(event: .viewOutageMapUnAuthOfferComplete)
         }
         
-        if urlString == nil {
+        if urlString?.isEmpty ?? true {
             let alertVc = UIAlertController(title: NSLocalizedString("Error", comment: ""), message: NSLocalizedString("The map is currently unavailable. Please try again later.", comment: ""), preferredStyle: .alert)
             alertVc.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: { [weak self] _ in
                 self?.navigationController?.popViewController(animated: true)
