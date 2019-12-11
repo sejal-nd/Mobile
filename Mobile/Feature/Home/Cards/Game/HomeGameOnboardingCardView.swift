8110cc9f94cc63529200c287a5a8e0755d8b0670
Marc Shilling <mshilling@mindgrub.com>
2019-12-11 14:25:40 -0500

    animated onboarding card

--- Mobile/Feature/Home/Cards/Game/HomeGameOnboardingCardView.swift
+++ Mobile/Feature/Home/Cards/Game/HomeGameOnboardingCardView.swift
@@ -8,17 +8,20 @@
 
 import RxSwift
 import RxCocoa
+import Lottie
 
 class HomeGameOnboardingCardView: UIView {
     
     @IBOutlet private weak var clippingView: UIView!
+    @IBOutlet private weak var lottieView: UIView!
     @IBOutlet private weak var label: UILabel!
     @IBOutlet weak var letsGoButton: UIButton!
+    
+    var lottieAnimation: AnimationView?
 
     static func create() -> HomeGameOnboardingCardView {
         let view = Bundle.main.loadViewFromNib() as HomeGameOnboardingCardView
         view.styleViews()
-        //view.viewModel = viewModel
         return view
     }
     
@@ -35,5 +38,19 @@ class HomeGameOnboardingCardView: UIView {
         letsGoButton.tintColor = .actionBlue
         letsGoButton.titleLabel?.font = SystemFont.semibold.of(textStyle: .headline)
         letsGoButton.titleLabel?.text = NSLocalizedString("Let's Go!", comment: "")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        lottieAnimation?.stop()
+        lottieAnimation?.removeFromSuperview()
+        
+        lottieAnimation = AnimationView(name: "home_onboarding_card")
+        lottieAnimation!.frame = CGRect(x: 0, y: 0, width: lottieView.frame.size.width, height: lottieView.frame.size.height)
+        lottieAnimation!.loopMode = .loop
+        lottieAnimation!.contentMode = .scaleAspectFit
+        lottieView.addSubview(lottieAnimation!)
+        lottieAnimation!.play()
     }
 }
