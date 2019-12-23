a88e24a5592482c19b065788e400637c040d4a7b
Marc Shilling <mshilling@mindgrub.com>
2019-12-23 10:49:08 -0500

    refactor GiftInventory so manual point entry is not necessary

--- Mobile/Core/GiftInventory.swift
+++ Mobile/Core/GiftInventory.swift
@@ -60,60 +60,60 @@ struct Gift {
 class GiftInventory {
 
     static let shared = GiftInventory()
-
-    private let inventory: [Gift] = [
-        Gift(id: "bg_bay", type: .background, requiredPoints: 16),
-        Gift(id: "hat_maryland", type: .hat, requiredPoints: 32),
-        Gift(id: "bg_snow", type: .background, requiredPoints: 48),
-        Gift(id: "hat_antlers", type: .hat, requiredPoints: 64),
-        Gift(id: "hat_beaniegreen", type: .hat, requiredPoints: 80),
-        Gift(id: "acc_hornrimmed", type: .accessory, requiredPoints: 96),
-        Gift(id: "bg_mdresidential", type: .background, requiredPoints: 112),
-        Gift(id: "hat_bgehardhat", type: .hat, requiredPoints: 128, customMessage: "Safety first!"),
-        Gift(id: "acc_safetyglasses", type: .accessory, requiredPoints: 144),
-        Gift(id: "bg_cherryblossoms", type: .background, requiredPoints: 160),
-        Gift(id: "hat_sprout", type: .hat, requiredPoints: 176),
-        Gift(id: "hat_flowercrown", type: .hat, requiredPoints: 192),
-        Gift(id: "acc_cateye", type: .accessory, requiredPoints: 208),
-        Gift(id: "hat_antlers", type: .hat, requiredPoints: 224),
-        Gift(id: "bg_skipjack", type: .background, requiredPoints: 240),
-        Gift(id: "hat_antlers", type: .hat, requiredPoints: 256),
-        Gift(id: "hat_crab", type: .hat, requiredPoints: 272),
-        Gift(id: "acc_sunglasses", type: .accessory, requiredPoints: 288),
-        Gift(id: "hat_sailor", type: .hat, requiredPoints: 304),
-        Gift(id: "bg_susans", type: .background, requiredPoints: 320),
-        Gift(id: "hat_sunhat", type: .hat, requiredPoints: 336),
-        Gift(id: "hat_fancy", type: .hat, requiredPoints: 352),
-        Gift(id: "hat_beretred", type: .hat, requiredPoints: 368),
-        Gift(id: "acc_glasses", type: .accessory, requiredPoints: 384),
-        Gift(id: "bg_forest", type: .background, requiredPoints: 400),
-        Gift(id: "hat_oriole", type: .hat, requiredPoints: 416),
-        Gift(id: "hat_dog", type: .hat, requiredPoints: 432, customMessage: "Woof!"),
-        Gift(id: "hat_cat", type: .hat, requiredPoints: 448, customMessage: "Meow!"),
-        Gift(id: "bg_farm", type: .background, requiredPoints: 464),
-        Gift(id: "hat_strawhat", type: .hat, requiredPoints: 480),
-        Gift(id: "hat_horse", type: .hat, requiredPoints: 496, customMessage: "Neigh!"),
-        Gift(id: "hat_rodent", type: .hat, requiredPoints: 512),
-        Gift(id: "hat_rabbit", type: .hat, requiredPoints: 528),
-        // Baltimore Harbor BG - 544
-        Gift(id: "hat_beaniefootball", type: .hat, requiredPoints: 560),
-        Gift(id: "hat_capbaseball", type: .hat, requiredPoints: 576, customMessage: "Play ball!"),
-        // Annapolis BG - 592
-        Gift(id: "hat_tophat", type: .hat, requiredPoints: 608),
-        Gift(id: "acc_monocle", type: .accessory, requiredPoints: 624),
-        Gift(id: "hat_bow", type: .hat, requiredPoints: 640),
-        // Elicott City BG - 656
-        Gift(id: "hat_capgreen", type: .hat, requiredPoints: 672),
-        // Striped Bow Hat - 688
-        // Historic MD BG - 704
-        Gift(id: "hat_colonial", type: .hat, requiredPoints: 720, customMessage: "How do you do?"),
-        Gift(id: "hat_cowboy", type: .hat, requiredPoints: 736, customMessage: "Howdy!"),
-        Gift(id: "bg_falltrees", type: .background, requiredPoints: 752),
-        Gift(id: "hat_beretblack", type: .hat, requiredPoints: 768),
-        Gift(id: "hat_paper", type: .hat, requiredPoints: 784),
-        // Space BG - 800
-        // Eco Crown Hat - 816
+    
+    private let orderedAssetIds = [
+        "bg_bay", "hat_maryland", "bg_snow", "hat_antlers", "hat_beaniegreen", "acc_hornrimmed",
+        "bg_mdresidential", "hat_bgehardhat", "acc_safetyglasses", "bg_cherryblossoms", "hat_sprout",
+        "hat_flowercrown", "acc_cateye", "bg_skipjack", "hat_crab", "acc_sunglasses", "hat_sailor",
+        "bg_susans", "bg_susans", "hat_fancy", "hat_beretred", "acc_glasses", "bg_forest",
+        "hat_oriole", "hat_dog", "hat_cat", "bg_farm", "hat_strawhat", "hat_horse", "hat_rodent",
+        "hat_rabbit",
+        // Baltimore Harbor
+        "hat_beaniefootball", "hat_capbaseball",
+        // Annapolis BG
+        "hat_tophat", "acc_monocle", "hat_bow",
+        // Elicott City BG
+        "hat_capgreen", "hat_colonial", "hat_cowboy", "bg_falltrees", "hat_beretblack", "hat_paper"
+        // Space BG
+        // Eco Crown Hat
     ]
+    
+    private let customMessages = [
+        "hat_bgehardhat": "Safety first!",
+        "hat_dog": "Woof!",
+        "hat_cat": "Meow!",
+        "hat_horse": "Neigh!",
+        "hat_capbaseball": "Play ball!",
+        "hat_colonial": "How do you do?",
+        "hat_cowboy": "Howdy!"
+    ]
+    
+    private let inventory: [Gift]!
+    
+    init() {
+        var inventory = [Gift]()
+        
+        var requiredPoints: Double = 16
+        for id in orderedAssetIds {
+            let type: GiftType
+            switch id.split(separator: "_").first {
+            case "bg":
+                type = .background
+            case "hat":
+                type = .hat
+            case "acc":
+                type = .accessory
+            default:
+                fatalError("Could not map GiftType")
+            }
+            
+            inventory.append(Gift(id: id, type: type, requiredPoints: requiredPoints, customMessage: customMessages[id]))
+                
+            requiredPoints += 16
+        }
+        
+        self.inventory = inventory
+    }
     
     func giftUnlockedWhen(pointsBefore: Double, pointsAfter: Double) -> Gift? {
         let giftsUnlockedBefore = inventory.filter { pointsBefore >= $0.requiredPoints }
