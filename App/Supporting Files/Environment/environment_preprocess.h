eda5c3c66bd397289ee94f3ac2b9db55d4444086
Joey Erlandson <joseph.erlandson@exeloncorp.com>
2020-05-19 09:20:37 -0400

    - Updated gitignore - removed lingering files - removed mistakenly tracked generated files

--- App/Supporting Files/Environment/environment_preprocess.h
+++
@@ -1,8 +0,0 @@
-//-----------------------------------------
-// Auto generated file
-// Created 
-//-----------------------------------------
-
-#define EXM_ENVIRONMENT              DEVELOP
-#define EXM_ENVIRONMENT_LAST_COMMIT  
-#define EXM_ENVIRONMENT_APP_NAME     
