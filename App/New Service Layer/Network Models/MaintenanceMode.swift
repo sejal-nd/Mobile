ee953465182592cb80b2477f2eebebb469d212c2
Joey Erlandson <joseph.erlandson@exeloncorp.com>
2020-07-14 16:14:02 -0400

    fixed maintenanceMode logic

--- App/New Service Layer/Network Models/MaintenanceMode.swift
+++ App/New Service Layer/Network Models/MaintenanceMode.swift
@@ -16,6 +16,7 @@ public struct MaintenanceMode: Decodable {
     public var usage = false
     public var home = false
     public var storm = false
+    public var alerts = false
     public var message = "The \(Environment.shared.opco.displayString) App is currently unavailable due to maintenance."
     
     enum CodingKeys: String, CodingKey {
@@ -25,6 +26,7 @@ public struct MaintenanceMode: Decodable {
         case outage = "outage"
         case usage = "usage"
         case home = "home"
+        case alerts = "alerts"
         case storm = "storm"
         case message = "message"
     }
@@ -33,13 +35,28 @@ public struct MaintenanceMode: Decodable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let iosContainer = try container.nestedContainer(keyedBy: CodingKeys.self,
                                                          forKey: .iOS)
-        self.all = try iosContainer.decode(Bool.self, forKey: .all)
-        self.bill = try iosContainer.decode(Bool.self, forKey: .bill)
-        self.outage = try iosContainer.decode(Bool.self, forKey: .outage)
-        self.usage = try iosContainer.decode(Bool.self, forKey: .usage)
-        self.home = try iosContainer.decode(Bool.self, forKey: .home)
-        self.storm = try iosContainer.decode(Bool.self, forKey: .storm)
-        self.all = try iosContainer.decode(Bool.self, forKey: .all)
+        
+        let iosAll = try iosContainer.decode(Bool.self, forKey: .all)
+        let iosBill = try iosContainer.decode(Bool.self, forKey: .bill)
+        let iosOutage = try iosContainer.decode(Bool.self, forKey: .outage)
+        let iosUsage = try iosContainer.decode(Bool.self, forKey: .usage)
+        let iosHome = try iosContainer.decode(Bool.self, forKey: .home)
+        let iosStorm = try iosContainer.decode(Bool.self, forKey: .storm)
+        
+        let rootAll = try container.decode(Bool.self, forKey: .all)
+        let rootBill = try container.decode(Bool.self, forKey: .bill)
+        let rootOutage = try container.decode(Bool.self, forKey: .outage)
+        let rootUsage = try container.decode(Bool.self, forKey: .usage)
+        let rootHome = try container.decode(Bool.self, forKey: .home)
+        let rootStorm = try container.decode(Bool.self, forKey: .storm)
+        self.alerts = try container.decode(Bool.self, forKey: .alerts)
+        
+        self.all = iosAll || rootAll
+        self.bill = iosBill || rootBill
+        self.outage = iosOutage || rootOutage
+        self.usage = iosUsage || rootUsage
+        self.home = iosHome || rootHome
+        self.storm = iosStorm || rootStorm
         self.message = try iosContainer.decode(String.self, forKey: .message)
     }
     
