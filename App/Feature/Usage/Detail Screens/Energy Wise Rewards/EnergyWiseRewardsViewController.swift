13da31df09a30d66baabb400a019ef3136b3a204
Majumdar, Amit (US - Bengaluru) <ammajumdar@deloitte.com>
2020-09-17 18:28:48 +0530

    Properties added

--- App/Feature/Usage/Detail Screens/Energy Wise Rewards/EnergyWiseRewardsViewController.swift
+++ App/Feature/Usage/Detail Screens/Energy Wise Rewards/EnergyWiseRewardsViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class EnergyWiseRewardsViewController: DismissableFormSheetViewController {
 
+    @IBOutlet private weak var logo: UIImageView!
+    @IBOutlet private weak var energyWiseRewardsInformationLabel: UILabel!
+    @IBOutlet private weak var adjustThermostatButton: PrimaryButton!
+    
     var accountDetail: AccountDetail!
     
     // MARK: - View LifeCycle Methods
@@ -24,14 +30,10 @@ class EnergyWiseRewardsViewController: DismissableFormSheetViewController {
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
 
-    /*
+   
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
 
 }
