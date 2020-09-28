a98df8eea0c7431786aa98f3271eafdd40a7b40b
Majumdar, Amit (US - Bengaluru) <ammajumdar@deloitte.com>
2020-09-28 12:01:33 +0530

    View Model Implementation Changes

--- App/Feature/Usage/Detail Screens/PeakEnergySavings/PeakEnergySavingsViewModel.swift
+++ App/Feature/Usage/Detail Screens/PeakEnergySavings/PeakEnergySavingsViewModel.swift
@@ -11,28 +11,28 @@ import Foundation
 final class PeakEnergySavingsViewModel {
     
     let accountDetail: AccountDetail
-    var opcoType: OpCo?
+    var subOpco: SubOpCo?
     
     init(accountDetail: AccountDetail) {
         self.accountDetail = accountDetail
-        if let opco = accountDetail.opcoType {
-            opcoType = opco
+        if let opco = accountDetail.subOpco {
+            subOpco = opco
         }
     }
-
+    
     var programDescription: String {
-        switch opcoType {
-        case .pepco:
+        switch subOpco {
+        case .pepcoMaryland:
             return NSLocalizedString("A Peak Savings Day is scheduled for Pepco customers in Maryland on Friday, July 19, 2020, from 01:00 PM to 05:00 PM.", comment: "")
-        case .delmarva:
-            return accountDetail.subOpco == .delmarvaDelaware ?
-                NSLocalizedString("A Peak Savings Day is scheduled for Delmarva Power customers in Delaware on Friday, July 19, 2020, from 01:00 PM to 05:00 PM.", comment: "") :
-                NSLocalizedString("A Peak Savings Day is scheduled for Delmarva Power customers in Maryland on Friday, July 19, 2020, from 01:00 PM to 05:00 PM.", comment: "")
-        default:
-            return "No Peak Savings day is currently scheduled"
+        case .delmarvaDelaware:
+            return NSLocalizedString("A Peak Savings Day is scheduled for Delmarva Power customers in Delaware on Friday, July 19, 2020, from 01:00 PM to 05:00 PM.", comment: "")
+        case .delmarvaMaryland:
+            return NSLocalizedString("A Peak Savings Day is scheduled for Delmarva Power customers in Maryland on Friday, July 19, 2020, from 01:00 PM to 05:00 PM.", comment: "")
+        case .none:
+            return ""
         }
     }
-
+    
     var programSuggestion: String {
         return NSLocalizedString("Reduce your energy usage below 20,000 kWh during the Peak Savings Day on Friday, July 19, 2020, from 01:00 PM to 05:00 PM. The more you reduce, the greater the opportunity to earn a credit.", comment: "")
     }
