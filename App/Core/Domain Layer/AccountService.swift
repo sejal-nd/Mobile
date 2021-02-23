1af567acb521264e58c46125ec582afa0ba9e68c
cdillon <cdillon@bemoreinnovations.com>
2021-02-23 08:54:42 -0500

    Revert "adding new account details flag for removing mdm dependency to differentiate login calls"
    This reverts commit 54f2c7019fac6954beb8acdf58214465e19dd97f.

--- App/Core/Domain Layer/AccountService.swift
+++ App/Core/Domain Layer/AccountService.swift
@@ -34,7 +34,6 @@ enum AccountService {
                                     programs: Bool = true,
                                     budgetBilling: Bool = true,
                                     alertPreferenceEligibilities: Bool = false,
-                                    includeMDM: Bool = true,
                                     completion: @escaping (Result<AccountDetail, NetworkingError>) -> ()) {
         
         var queryItems = [URLQueryItem]()
@@ -50,10 +49,6 @@ enum AccountService {
         
         if alertPreferenceEligibilities {
             queryItems.append(URLQueryItem(name: "alertPreferenceEligibilities", value: "true"))
-        }
-        
-        if !includeMDM {
-            queryItems.append(URLQueryItem(name: "includeMDM", value: "false"))
         }
     
         NetworkingLayer.request(router: .accountDetails(accountNumber: accountNumber, queryItems: queryItems), completion: completion)
