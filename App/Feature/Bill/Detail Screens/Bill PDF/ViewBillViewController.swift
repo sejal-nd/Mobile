88e75760e6953209e7ce33bca1778e0286294eab
cdillon <cdillon@bemoreinnovations.com>
2021-05-11 11:03:54 -0400

    adding analytics call for current and past bill pdf not available

--- App/Feature/Bill/Detail Screens/Bill PDF/ViewBillViewController.swift
+++ App/Feature/Bill/Detail Screens/Bill PDF/ViewBillViewController.swift
@@ -80,8 +80,13 @@ class ViewBillViewController: UIViewController {
             guard let self = self else { return }
             self.setupWKWebView()
         }, onError: { [weak self] errMessage in
-//            TODO need to differentiate between .current_pdf_not_available and .past_pdf_not_available webview delegate if url fails
-//            FirebaseUtility.logEventV2(.bill(parameters: [.past_pdf_not_available]))
+            
+            if self?.viewModel.isCurrent == true {
+                FirebaseUtility.logEventV2(.bill(parameters: [.current_pdf_not_available]))
+            } else {
+                FirebaseUtility.logEventV2(.bill(parameters: [.past_pdf_not_available]))
+            }
+
             guard let self = self else { return }
             self.loadingIndicator.isHidden = true
             self.errorLabel.isHidden = false
