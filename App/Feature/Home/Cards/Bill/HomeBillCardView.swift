8ae5e9b8f508036fcd88b03befdfea461d82de3d
Adarsh <admaurya@deloitte.com>
2021-07-21 16:55:41 +0530

    BGE Assistance CTA

--- App/Feature/Home/Cards/Bill/HomeBillCardView.swift
+++ App/Feature/Home/Cards/Bill/HomeBillCardView.swift
@@ -242,6 +242,9 @@ class HomeBillCardView: UIView {
     
     private func bindViewModel() {
         viewBillButton.isHidden = !viewModel.showViewBillButton
+        if Configuration.shared.opco == .bge {
+            self.assistanceView.isHidden = true
+        }
         
         viewModel.paymentTracker.asDriver().drive(onNext: {
             if $0 {
@@ -251,6 +254,8 @@ class HomeBillCardView: UIView {
             }
         })
             .disposed(by: bag)
+        
+        viewModel.fetchBGEDdeDpaEligibility.asDriver().drive().disposed(by: bag)
         
         viewModel.showLoadingState
             .drive(onNext: { _ in UIAccessibility.post(notification: .screenChanged, argument: nil) })
@@ -327,6 +332,12 @@ class HomeBillCardView: UIView {
             guard let self = self else { return }
             if description == nil {
                 self.assistanceView.isHidden = true
+            }
+            if (description?.title == "") &&
+                (description?.description == "") {
+                self.assistanceView.isHidden = true
+            } else {
+                self.assistanceView.isHidden = false
             }
             DispatchQueue.main.async {
                 if description?.ctaType == "Reinstate Payment Arrangement" {
