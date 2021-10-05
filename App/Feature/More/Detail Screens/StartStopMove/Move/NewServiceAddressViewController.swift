1b7001ddcb21d3807583ef11fa3c80bda1fb6ed4
Mithlesh Kumar <C996752@exelonds.com>
2021-10-05 16:57:33 +0530

    Changes made as per review comment:
    1. zipcode while editing continue button will scrollup
    2. street address and appartment unit won't trigger untill zip is verified
    3. scroll screen is changed to scrollview

--- App/Feature/More/Detail Screens/StartStopMove/Move/NewServiceAddressViewController.swift
+++ App/Feature/More/Detail Screens/StartStopMove/Move/NewServiceAddressViewController.swift
@@ -10,20 +10,33 @@ import UIKit
 import RxCocoa
 import RxSwift
 
-class NewServiceAddressViewController: UIViewController {
+class NewServiceAddressViewController: KeyboardAvoidingStickyFooterViewController {
     @IBOutlet weak var zipTextField: FloatLabelTextField!
-    @IBOutlet weak var streetAddressTextField: FloatLabelTextField!
-    @IBOutlet weak var unit_app_TextField: FloatLabelTextField!
     @IBOutlet weak var continueButton: PrimaryButton!
     @IBOutlet weak var loadingIndicator: LoadingIndicator!
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    @IBOutlet weak var streetAddressPlaceHolderLabel: UILabel!
+    @IBOutlet weak var selectedstreetAddressStackView: UIStackView!
+    @IBOutlet weak var selectedstreetAddressLabel: UILabel!
+    @IBOutlet weak var streetAddressFloatingLabel: UILabel!
+    @IBOutlet weak var streetAddressSelectionView: UIView!
+
+    @IBOutlet weak var appUnitPlaceHolderLabel: UILabel!
+    @IBOutlet weak var selectedAppUnitStackView: UIStackView!
+    @IBOutlet weak var selectedAppUnitLabel: UILabel!
+    @IBOutlet weak var appUnitFloatingLabel: UILabel!
+    @IBOutlet weak var appUnitSelectionView: UIView!
+
     var disposeBag = DisposeBag()
 
     private var viewModel = NewServiceAddressViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationItem.backBarButtonItem?.title = ""
         // Do any additional setup after loading the view.
 
+       
         configureTextField()
         setupUI()
 
@@ -35,9 +48,6 @@ class NewServiceAddressViewController: UIViewController {
 
     private func configureTextField() {
         zipTextField.placeholder = NSLocalizedString("Zip Code*", comment: "")
-        streetAddressTextField.placeholder = NSLocalizedString("Street Address*", comment: "")
-        unit_app_TextField.placeholder = NSLocalizedString("Apt/Unit #*", comment: "")
-
 
         zipTextField.textField.delegate = self
         zipTextField.textField.keyboardType = .numberPad
@@ -45,32 +55,53 @@ class NewServiceAddressViewController: UIViewController {
         zipTextField.textField.isShowingAccessory = true
         zipTextField.textField.textContentType = .postalCode
 
+        streetAddressPlaceHolderLabel.font = SystemFont.regular.of(textStyle: .callout)
+        streetAddressPlaceHolderLabel.textColor = .middleGray
+        streetAddressPlaceHolderLabel.text = NSLocalizedString("Street Address*", comment: "")
 
+        streetAddressFloatingLabel.font = SystemFont.semibold.of(textStyle: .caption2)
+        streetAddressFloatingLabel.textColor = .middleGray
 
+        selectedstreetAddressLabel.font = SystemFont.regular.of(textStyle: .callout)
+        selectedstreetAddressLabel.textColor = .middleGray
+        changeStreetColorState(isActive: false)
 
-        streetAddressTextField.textField.delegate = self
-        streetAddressTextField.setKeyboardType(.default, doneActionTarget: self, doneActionSelector: #selector(streetAddressDonePressed))
-        zipTextField.textField.isShowingAccessory = true
-        zipTextField.textField.textContentType = .addressCityAndState
+        appUnitPlaceHolderLabel.font = SystemFont.regular.of(textStyle: .callout)
+        appUnitPlaceHolderLabel.textColor = .middleGray
+        appUnitPlaceHolderLabel.text = NSLocalizedString("Apt/Unit #*", comment: "")
 
+        appUnitFloatingLabel.font = SystemFont.semibold.of(textStyle: .caption2)
+        appUnitFloatingLabel.textColor = .middleGray
 
-        unit_app_TextField.textField.delegate = self
-        unit_app_TextField.setKeyboardType(.default, doneActionTarget: self, doneActionSelector: #selector(streetAddressDonePressed))
-        unit_app_TextField.textField.isShowingAccessory = true
-        unit_app_TextField.textField.textContentType = .addressCityAndState
+        selectedAppUnitLabel.font = SystemFont.regular.of(textStyle: .callout)
+        selectedAppUnitLabel.textColor = .middleGray
+
+        changeAppUnitColorState(isActive: false)
 
     }
     private func setupUI() {
         viewModel.showLoadingState
             .subscribe (onNext: { [weak self] status in
-                self?.loadingIndicator.isHidden = !status
+                  self?.loadingIndicator.isHidden = !status
             }).disposed(by: disposeBag)
-
 
         continueButton.isEnabled = viewModel.canEnableContinue
     }
 
-
+    private func changeStreetColorState(isActive : Bool) {
+        if (isActive){
+            streetAddressSelectionView.roundCorners(.allCorners, radius: 10.0, borderColor:.primaryColor, borderWidth: 1.0)
+        }else {
+            streetAddressSelectionView.roundCorners(.allCorners, radius: 10.0, borderColor:.accentGray, borderWidth: 1.0)
+        }
+    }
+    private func changeAppUnitColorState(isActive : Bool) {
+        if (isActive){
+            appUnitSelectionView.roundCorners(.allCorners, radius: 10.0, borderColor:.primaryColor, borderWidth: 1.0)
+        }else {
+            appUnitSelectionView.roundCorners(.allCorners, radius: 10.0, borderColor:.accentGray, borderWidth: 1.0)
+        }
+    }
     @objc func zipCodeDonePressed() {
         zipTextField.textField.resignFirstResponder()
         if !viewModel.isZipValid {
@@ -78,39 +109,38 @@ class NewServiceAddressViewController: UIViewController {
         }else {
             viewModel.validateZip()
         }
+        scrollView.setContentOffset(CGPoint(x: 0, y: scrollView.contentInset.top), animated: true)
     }
-    @objc func streetAddressDonePressed() {
+    @IBAction func streetAddressPressed(_ sender: Any) {
         zipTextField.textField.resignFirstResponder()
         if !viewModel.isZipValid {
             zipTextField.setError(NSLocalizedString("Zip  Code must be 5 characters in length", comment: ""))
+        } else if !viewModel.isZipValidated {
+            viewModel.validateZip()
         }else {
-           //
+            // navigate to street search
 
         }
-
     }
+    
+    @IBAction func appUnitPressed(_ sender: Any) {
+        zipTextField.textField.resignFirstResponder()
+        if !viewModel.isZipValid {
+            zipTextField.setError(NSLocalizedString("Zip  Code must be 5 characters in length", comment: ""))
+        }
+        else if (!viewModel.isZipValidated && !viewModel.isStreetAddressValid ){
+
+        }else {
+            // navigate to appartment unit search
+        }
+    }
+    
 }
 extension NewServiceAddressViewController: UITextFieldDelegate {
-
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        if textField == streetAddressTextField.textField {
-            if (!viewModel.isZipValid && !viewModel.isZipValidated){
-                return false
-            }
-        }
-        else if textField == unit_app_TextField.textField {
-            if (!viewModel.isZipValid && !viewModel.isZipValidated && !viewModel.isStreetAddressValid ){
-                return false
-            }
-        }
-        return true
-    }
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let newString = (textField.text! as NSString).replacingCharacters(in: range, with: string)
 
-        if textField == streetAddressTextField.textField {
-            viewModel.streetAddress = newString
-        } else if textField == zipTextField.textField {
+        if textField == zipTextField.textField {
             let components = newString.components(separatedBy: CharacterSet.decimalDigits.inverted)
             let decimalString = components.joined(separator: "") as String
             let length = decimalString.count
@@ -134,19 +164,9 @@ extension NewServiceAddressViewController: UITextFieldDelegate {
     }
 
     func textFieldDidEndEditing(_ textField: UITextField) {
-        if textField == streetAddressTextField.textField {
-            unit_app_TextField.textField.becomeFirstResponder()
-        } else {
-            textField.resignFirstResponder()
-        }
         continueButton.isEnabled = viewModel.canEnableContinue
     }
 
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        if textField == streetAddressTextField.textField {
-            streetAddressTextField.setError(nil)
-        } else if textField == zipTextField.textField {
-            zipTextField.setError(nil)
-        }
     }
 }
