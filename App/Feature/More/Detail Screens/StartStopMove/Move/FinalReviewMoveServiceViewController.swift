650e753d5b5c0d9005406d3350d959714d39c980
Mithlesh Kumar <C996752@exelonds.com>
2021-11-10 21:10:17 +0530

    compiling error fix

--- App/Feature/More/Detail Screens/StartStopMove/Move/FinalReviewMoveServiceViewController.swift
+++ App/Feature/More/Detail Screens/StartStopMove/Move/FinalReviewMoveServiceViewController.swift
@@ -253,7 +253,7 @@ class FinalReviewMoveServiceViewController: UIViewController {
 
         alertController.addAction(doneAction)
 
-        if let popoverController = actionSheet.popoverPresentationController { // iPad popover
+        if let popoverController = alertController.popoverPresentationController { // iPad popover
             let width = self.changeRenterOwnerButton.frame.size.width
             popoverController.sourceView = self.changeRenterOwnerButton
             popoverController.sourceRect = CGRect(x: width / 2, y: 0, width: 0, height: 0)
