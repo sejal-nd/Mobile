56c267a6649f92ca0b884df0c313fd9fccbc501c
Mithlesh Kumar <C996752@exelonds.com>
2021-11-13 13:21:28 +0530

    implementation for User Story 931928: TECH-UI: Update Estimated Time for Stop Service Landing Screen - iOS

--- App/Feature/More/Detail Screens/StartStopMove/Stop/StopLandingViewController.swift
+++ App/Feature/More/Detail Screens/StartStopMove/Stop/StopLandingViewController.swift
@@ -25,6 +25,7 @@ class StopLandingViewController: UIViewController {
             estimatedTimeLabel.font = SystemFont.regular.of(textStyle: .caption1)
             estimatedTimeLabel.text = NSLocalizedString("EST. 2-4 MINUTES", comment: "")
             estimatedTimeLabel.accessibilityLabel = NSLocalizedString("Estimated Time 2 to 4 minutes", comment: "")
+
         }
     }
     @IBOutlet weak var youWillNeedLabel: UILabel! {
