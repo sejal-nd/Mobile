84a70ed08592b3f6bc315ba1b8bf7ad8d722ef1d
Mithlesh Kumar <C996752@exelonds.com>
2021-11-16 12:39:42 +0530

    minor change to fix conflict and push new build

--- App/Feature/More/Detail Screens/StartStopMove/Move/FinalReviewMoveServiceViewController.swift
+++ App/Feature/More/Detail Screens/StartStopMove/Move/FinalReviewMoveServiceViewController.swift
@@ -201,7 +201,7 @@ class FinalReviewMoveServiceViewController: UIViewController {
 
         self.finalBillAddressStackView.isHidden = moveFlowData.currentAccountDetail?.isEBillEnrollment ?? false
         self.ebillStackView.isHidden = !(moveFlowData.currentAccountDetail?.isEBillEnrollment ?? false)
-        self.ebillUserInfoLabel.text = "The bill for service at your previous address will be delivered to \(moveFlowData.currentAccountDetail.customerInfo.emailAddress ?? "")."
+        self.ebillUserInfoLabel.text = "The bill for service at your previous address will be delivered to \(moveFlowData.currentAccountDetail?.customerInfo.emailAddress ?? "")."
         self.supplierAgreementStackView.isHidden = !(moveFlowData.currentAccountDetail?.hasThirdPartySupplier ?? false)
 
         self.submitBtn.isUserInteractionEnabled = !(moveFlowData.currentAccountDetail?.hasThirdPartySupplier ?? false)
