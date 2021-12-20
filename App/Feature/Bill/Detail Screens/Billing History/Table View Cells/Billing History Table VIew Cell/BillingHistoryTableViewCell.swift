c2ad475356d79b5ea8d7d1e48d685354832a6699
Joy Anne Abarzosa <C998719@exelonds.com>
2021-12-20 06:38:14 +0000

    Updated BillingHistoryTableViewCell.swift

--- App/Feature/Bill/Detail Screens/Billing History/Table View Cells/Billing History Table VIew Cell/BillingHistoryTableViewCell.swift
+++ App/Feature/Bill/Detail Screens/Billing History/Table View Cells/Billing History Table VIew Cell/BillingHistoryTableViewCell.swift
@@ -45,7 +45,7 @@ class BillingHistoryTableViewCell: UITableViewCell {
             iconImageView.image = #imageLiteral(resourceName: "ic_bill")
             let titleText = NSLocalizedString("Bill Issued", comment: "")
             titleLabel.text = titleText
-            amountLabel.text = item.totalAmountDue?.currencyString
+            amountLabel.text = item.totalAmountDue?.currencyString ?? Double(0.00).currencyString
             a11y = String(format: NSLocalizedString("%@. %@. %@. View PDF", comment: ""), titleText, dateString, amountLabel.text ?? "")
         } else {
             guard let amountPaid = item.amountPaid?.currencyString else {
