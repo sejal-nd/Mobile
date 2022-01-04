b360d9f38feea1db42120bfcf3d1c8354ac7d86a
cdillon <cdillon@bemoreinnovations.com>
2022-01-04 14:34:13 -0500

    fix for 2sv settings toast message not showing

--- App/Feature/More/MoreViewController.swift
+++ App/Feature/More/MoreViewController.swift
@@ -393,13 +393,13 @@ extension MoreViewController: UITableViewDataSource, UITableViewDelegate {
                            let json = TokenResponse.decodeToJson(token: result),
                            let editAction = json["profileEditActionTaken"] as? String {
                             
-                            if editAction.contains("mfa") {
-                                self.view.showToast("Two-Step Verification settings updated")
-                            } else if editAction.contains("password") {
+                            if editAction.contains("password") {
                                 self.view.showToast("Password changed")
+                            } else {
+                                self.view.showToast("Two-Step Verification settings updated")
                             }
                         } else {
-                            // TODO display error message?
+                            self.view.showToast("Error updating security credentials.")
                         }
                     }
                 } else {
