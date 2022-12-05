cd3afe0778673b0337d7dbff9aadd94bdc52f8ec
Anurag Tiwari <c995171@exelonds.com>
2022-12-05 18:40:12 +0530

    Security Exception - full account num removal from Registration flow

--- App/Core/Service Layer/Network Models/AccountResult.swift
+++ App/Core/Service Layer/Network Models/AccountResult.swift
@@ -9,13 +9,23 @@
 import Foundation
 
 struct AccountResult: Decodable {
-    let accountNumber: String?
+    let maskedAccountNumber: String?
+    let fullAccountNumber: String?
     let customerID: String?
     let streetNumber: String?
     let unitNumber: String?
     
+    var accountNumber: String? {
+        if fullAccountNumber?.isEmpty == false {
+            return fullAccountNumber
+        } else {
+            return maskedAccountNumber
+        }
+    }
+    
     enum CodingKeys: String, CodingKey {
-        case accountNumber = "fullAccountNumber"
+        case maskedAccountNumber = "accountNumber"
+        case fullAccountNumber = "fullAccountNumber"
         case customerID = "customerID"
         case streetNumber = "street"
         case unitNumber = "unit"
