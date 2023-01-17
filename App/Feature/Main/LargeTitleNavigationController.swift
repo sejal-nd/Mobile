06936331098ca34cd7a77471dce751e6a247acac
cdillon <C091362@exelonds.com>
2023-01-17 13:48:57 -0500

    adding excluded archs for simulator. image and color rebrand updates

--- App/Feature/Main/LargeTitleNavigationController.swift
+++ App/Feature/Main/LargeTitleNavigationController.swift
@@ -43,7 +43,7 @@ class LargeTitleNavigationController: UINavigationController {
             ]
             
             if StormModeStatus.shared.isOn { // No bottom border in Storm Mode
-                appearance.shadowColor = .clear
+                appearance.shadowImage = UIColor.secondaryGreen.navBarShadowImage()
             } else {
                 appearance.shadowImage = UIColor.primaryColor.navBarShadowImage()
             }
@@ -55,7 +55,7 @@ class LargeTitleNavigationController: UINavigationController {
             navigationBar.setBackgroundImage(nil, for: .default)
             
             if StormModeStatus.shared.isOn { // No bottom border in Storm Mode
-                navigationBar.shadowImage = UIImage()
+                navigationBar.shadowImage = UIColor.secondaryGreen.navBarShadowImage()
             } else {
                 navigationBar.shadowImage = UIColor.primaryColor.navBarShadowImage()
             }
