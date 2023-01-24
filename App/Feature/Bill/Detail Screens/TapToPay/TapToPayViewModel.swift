0f6368831b95e2e02797f634a21b31473cb105bc
Patricia Louise Goyal <C967977@exelonds.com>
2023-01-24 01:56:55 -0500

    added checker for past due amount

--- App/Feature/Bill/Detail Screens/TapToPay/TapToPayViewModel.swift
+++ App/Feature/Bill/Detail Screens/TapToPay/TapToPayViewModel.swift
@@ -278,9 +278,11 @@ class TapToPayViewModel {
         }
         
         // If not one of the above precarious states...
-        if Configuration.shared.opco == .bge || Configuration.shared.opco.isPHI { // BGE can always future date adding PHI for future date
+        if Configuration.shared.opco == .bge { // BGE can always future date
             return true
-        } else { // ComEd/PECO can only future date if the due date has not passed
+        } else if Configuration.shared.opco.isPHI && billingInfo.pastDueAmount == 0 {// added PHI for future date
+            return true
+        }else { // ComEd/PECO can only future date if the due date has not passed
             return isDueDateInTheFuture
         }
     }
