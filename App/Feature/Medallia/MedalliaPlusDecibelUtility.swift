a44d82a8776b4c8ffaf6ee5ab348e76571423282
cdillon <C091362@exelonds.com>
2023-04-13 14:01:39 -0400

    showing medallia survery instead of survey monkey for outage tracker

--- App/Feature/Medallia/MedalliaPlusDecibelUtility.swift
+++ App/Feature/Medallia/MedalliaPlusDecibelUtility.swift
@@ -148,13 +148,21 @@ final class MedalliaPlusDecibelUtility: DecibelDelegate {
         MedalliaDigital.setCustomParameter(name: "NetAmountDue", value: netAmountDue)
     }
 
-    func showOutageTrackerSurvey() {
-        // TODO set custom parameters
+    func showOutageTrackerSurvey(for outage: OutageTracker) {
+        MedalliaDigital.setCustomParameters([
+            "CustomerID": AccountsStore.shared.customerIdentifier ?? "",
+            "ETR": outage.etr ?? "",
+            "ETR Type": outage.etrType ?? "",
+            "Outage Cause": outage.cause ?? "",
+            "Outage ID": outage.outageID ?? "",
+            "Tracker Status": outage.trackerStatus ?? "",
+            "Outage Valid": outage.isOutageValid
+        ])
+        
         MedalliaDigital.showForm("988") {
             Log.info("Outage Tracker Survey Displayed Successfully")
         } failure: { error in
             Log.error("Error displaying Outage Tracker Survey: \(error)")
         }
-
     }
 }
